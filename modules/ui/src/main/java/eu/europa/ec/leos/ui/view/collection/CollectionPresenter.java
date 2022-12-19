f7d5f68aa9cf94b1569d998b662b64cb57dc12df
Felipe CAMARGO MACHADO <camarfe@s4dad.aws.cloud.tech.ec.europa.eu>
2022-12-19 14:19:26 +0100

    Core-96 Fix to specific situation found in tests

--- modules/ui/src/main/java/eu/europa/ec/leos/ui/view/collection/CollectionPresenter.java
+++ modules/ui/src/main/java/eu/europa/ec/leos/ui/view/collection/CollectionPresenter.java
@@ -16,6 +16,7 @@ package eu.europa.ec.leos.ui.view.collection;
 import com.google.common.base.Stopwatch;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
+import eu.europa.ec.leos.cmis.mapping.CmisProperties;
 import eu.europa.ec.leos.domain.cmis.LeosCategory;
 import eu.europa.ec.leos.domain.cmis.LeosExportStatus;
 import eu.europa.ec.leos.domain.cmis.LeosPackage;
@@ -28,6 +29,7 @@ import eu.europa.ec.leos.domain.vo.DocumentVO;
 import eu.europa.ec.leos.domain.vo.MetadataVO;
 import eu.europa.ec.leos.i18n.MessageHelper;
 import eu.europa.ec.leos.integration.rest.UserJSON;
+import eu.europa.ec.leos.model.action.VersionVO;
 import eu.europa.ec.leos.model.event.ExportPackageCreatedEvent;
 import eu.europa.ec.leos.model.event.ExportPackageDeletedEvent;
 import eu.europa.ec.leos.model.event.ExportPackageUpdatedEvent;
@@ -71,6 +73,7 @@ import eu.europa.ec.leos.services.store.ExportPackageService;
 import eu.europa.ec.leos.services.store.PackageService;
 import eu.europa.ec.leos.services.store.TemplateService;
 import eu.europa.ec.leos.services.store.WorkspaceService;
+import eu.europa.ec.leos.services.support.VersionsUtil;
 import eu.europa.ec.leos.services.user.UserService;
 import eu.europa.ec.leos.ui.event.CloneProposalRequestEvent;
 import eu.europa.ec.leos.ui.event.CloseScreenRequestEvent;
@@ -111,6 +114,7 @@ import eu.europa.ec.leos.web.event.view.repository.ExplanatoryCreateWizardReques
 import eu.europa.ec.leos.web.event.view.repository.SupportingDocumentsCreateWizardRequestEvent;
 import eu.europa.ec.leos.web.event.window.SaveMetaDataRequestEvent;
 import eu.europa.ec.leos.web.model.UserVO;
+import eu.europa.ec.leos.web.model.VersionInfoVO;
 import eu.europa.ec.leos.web.support.SessionAttribute;
 import eu.europa.ec.leos.web.support.UuidHelper;
 import eu.europa.ec.leos.web.support.log.LogUtil;
@@ -139,10 +143,13 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -154,6 +161,7 @@ import java.util.concurrent.locks.StampedLock;
 import java.util.stream.Collectors;
 
 import static eu.europa.ec.leos.services.support.XmlHelper.XML_DOC_EXT;
+import static eu.europa.ec.leos.util.LeosDomainUtil.CMIS_PROPERTY_SPLITTER;
 
 @Component
 @Scope("prototype")
@@ -202,6 +210,7 @@ class CollectionPresenter extends AbstractLeosPresenter {
     private UserAuthentication userAuthentication;
     private final DocumentContentService documentContentService;
     private final ComparisonDelegate comparisonDelegate;
+    private final static SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
     @Value("${leos.clone.originRef}")
     private String cloneOriginRef;
@@ -747,6 +756,18 @@ class CollectionPresenter extends AbstractLeosPresenter {
         Annex annex = annexService.findAnnex(event.getAnnex().getId(), true);
         AnnexMetadata metadata = annex.getMetadata().getOrError(() -> "Annex metadata not found!");
         AnnexMetadata updatedMetadata = metadata.withTitle(event.getAnnex().getTitle());
+
+        String baseRevisionId = annex.getBaseRevisionId();
+        cloneContext.setCloneProposalMetadataVO(cloneProposalMetadataVO);
+
+        if(StringUtils.isBlank(baseRevisionId)) {
+            VersionVO versionVO = VersionsUtil.buildVersionVO(Arrays.asList(annex), messageHelper).get(0);
+            Map<String, Object> properties = new HashMap<>();
+            properties.put(CmisProperties.BASE_REVISION_ID.getId(), versionVO.getDocumentId() + CMIS_PROPERTY_SPLITTER +
+                    versionVO.getVersionNumber().toString() + CMIS_PROPERTY_SPLITTER +
+                    versionVO.getCheckinCommentVO().getTitle());
+            annex =  annexService.updateAnnex(annex.getId(), properties, true);
+        }
 
         // 2. save metadata
         annexService.updateAnnex(annex, updatedMetadata, VersionType.MINOR, messageHelper.getMessage("collection.block.annex.metadata.updated"));
