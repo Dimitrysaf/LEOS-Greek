ea51889fe4aa1d1b1c2145d9869c0c8f099d3730
Felipe CAMARGO MACHADO <felipe.camargo-machado@ext.ec.europa.eu>
2023-02-01 15:03:28 +0100

    Core #198 Fix to regression in style for preface title of annex in draft

---
+++ modules/services/src/main/java/eu/europa/ec/leos/services/document/TransformationProposalService.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023 European Commission
+ *
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ *     https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
+package eu.europa.ec.leos.services.document;
+
+import eu.europa.ec.leos.domain.cmis.LeosCategory;
+import eu.europa.ec.leos.domain.common.InstanceType;
+import eu.europa.ec.leos.instance.Instance;
+import eu.europa.ec.leos.security.LeosPermission;
+import freemarker.template.Configuration;
+import freemarker.template.TemplateHashModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.List;
+
+@Service
+@Instance(instances = {InstanceType.COMMISSION, InstanceType.OS})
+public class TransformationProposalService extends TransformationServiceImpl {
+
+    @Autowired
+    public TransformationProposalService(Configuration freemarkerConfiguration, TemplateHashModel enumModels){
+        super(freemarkerConfiguration, enumModels);
+    }
+
+    @Override
+    public String toEditableXml(final InputStream documentStream, String contextPath, LeosCategory category,
+            List<LeosPermission> permissions, InputStream coverPageStream) {
+        String template;
+        switch (category){
+            case ANNEX:
+            case COUNCIL_EXPLANATORY:
+            case MEMORANDUM:
+            case BILL:
+            case COVERPAGE:
+            case PROPOSAL:
+            case STAT_FINANC_LEGIS:
+                template = getEditableXHtmlTemplate();
+                break;
+            default:
+                throw new UnsupportedOperationException("No transformation supported for this category");
+        }
+        return transform(documentStream, template, contextPath, permissions, coverPageStream);
+    }
+
+}
---
+++ modules/services/src/main/resources/eu/europa/ec/leos/freemarker/templates/akn_xml_mapper_annex_revision.ftl
@@ -0,0 +1,31 @@
+<#ftl encoding="UTF-8"
+      output_format="XML"
+      auto_esc=true
+      strict_syntax=true
+      strip_whitespace=true
+      strip_text=true
+      ns_prefixes={"D":"http://docs.oasis-open.org/legaldocml/ns/akn/3.0",
+                   "leos":"urn:eu:europa:ec:leos",
+                    "xml":"http://www.w3.org/XML/1998/namespace"}>
+
+<#--
+    Copyright 2023 European Commission
+
+    Licensed under the EUPL, Version 1.2 or - as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+    You may not use this work except in compliance with the Licence.
+    You may obtain a copy of the Licence at:
+
+        https://joinup.ec.europa.eu/software/page/eupl
+
+    Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the Licence for the specific language governing permissions and limitations under the Licence.
+-->
+
+<#include "akn_xml_mapper.ftl" parse=true>
+
+<#macro preface>
+    <preface leos:origin="${.node["@leos:origin"][0]!}" xml:id="_preface" leos:imported="true">
+        <#recurse/>
+    </preface>
+</#macro>
