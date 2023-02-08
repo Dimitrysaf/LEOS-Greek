f6bdea58e25ffd445a4f225cd6fbab2c4096afa6
Nikos Mariolis <ngmariolis@gmail.com>
2023-02-08 23:48:32 +0200

    LEOSCOMM-206-implemented save version with modal. BE Error to resolve

--- modules/ui-angular/src/app/features/akn-document/components/annex-actions-dropdown/annex-actions-dropdown.component.ts
+++ modules/ui-angular/src/app/features/akn-document/components/annex-actions-dropdown/annex-actions-dropdown.component.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { EuiDialogComponent } from '@eui/components/eui-dialog';
+import { Subject } from 'rxjs';
 
-import { DocumentService } from '@/features/akn-document/services/document.service';
+import { DocumentService } from '@/shared/services/document.service';
 
 @Component({
   selector: 'app-annex-actions-dropdown',
   templateUrl: './annex-actions-dropdown.component.html',
   styleUrls: ['./annex-actions-dropdown.component.scss'],
 })
-export class AnnexActionsDropdownComponent {
-  constructor(public doc: DocumentService) {}
+export class AnnexActionsDropdownComponent implements OnInit, OnDestroy {
+  createForm: FormGroup;
+
+  @ViewChild('createVersionDialog') createVersionDialog: EuiDialogComponent;
+  private destroy$ = new Subject();
+
+  constructor(private fb: FormBuilder, public doc: DocumentService) {}
+
+  ngOnDestroy(): void {
+    this.destroy$.next(null);
+    this.destroy$.complete();
+  }
+
+  ngOnInit() {
+    this.createForm = this.fb.group({
+      title: new FormControl(
+        { value: '', disabled: false },
+        { validators: Validators.required },
+      ),
+      description: new FormControl(
+        { value: '', disabled: false },
+        { validators: Validators.required },
+      ),
+    });
+  }
+
+  openVersionModal() {
+    this.createVersionDialog.openDialog();
+  }
+
+  closeVersionModal() {
+    this.createVersionDialog.closeDialog();
+  }
+
+  onSaveVersion() {
+    const requestBody = this.populateDataForVersionSave();
+    this.doc.saveVersion(requestBody).subscribe((response) => {
+      this.closeVersionModal();
+    });
+  }
+
+  private populateDataForVersionSave() {
+    //TODO when version Type is defined refactor this.
+    const { title1, description1 } = this.createForm.getRawValue();
+    return {
+      checkinComment: JSON.stringify({
+        title: title1,
+        description: description1,
+      }),
+      versionType: 'INTERMEDIATE',
+    };
+  }
 }
