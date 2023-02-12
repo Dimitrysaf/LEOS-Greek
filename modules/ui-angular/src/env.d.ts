68bda17cf7f8055a28935b47383dd123c3dbb94e
Nikos Mariolis <ngmariolis@gmail.com>
2023-02-12 13:57:09 +0200

    LEOSCOMM-228-footer populated with data from env variables. Configuration needed in order to fill the env variables at build time.

--- modules/ui-angular/src/env.d.ts
+++ modules/ui-angular/src/env.d.ts
@@ -3,5 +3,7 @@ declare namespace NodeJS {
     NG_APP_ENV: string;
     NG_APP_LEOS_INSTANCE: string;
     NG_APP_LEOS_ENV: string;
+    NG_APP_LEOS_VERSION: string;
+    NG_APP_LEOS_VERSION_BUILD_DATE: string;
   }
 }
