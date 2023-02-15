8db253137dc7242daeb152f11de0f390851ed586
Felipe CAMARGO MACHADO <camarfe@s4dad.aws.cloud.tech.ec.europa.eu>
2023-02-15 13:44:15 +0100

    Core-279 Removed message box from financial statement

--- modules/ui/src/main/java/eu/europa/ec/leos/ui/view/financialstatement/FinancialStatementPresenter.java
+++ modules/ui/src/main/java/eu/europa/ec/leos/ui/view/financialstatement/FinancialStatementPresenter.java
@@ -651,8 +651,6 @@ public class FinancialStatementPresenter extends AbstractLeosPresenter {
         financialStatement = financialStatementService.updateFinancialStatement(financialStatement, updatedXmlContent,
                 VersionType.MINOR, messageHelper.getMessage("operation.financial.block.inserted"));
         if (financialStatement != null) {
-            eventBus.post(new NotificationEvent(NotificationEvent.Type.INFO, "document.financial.block.inserted",
-                    StringUtils.capitalize(tagName)));
             eventBus.post(new RefreshDocumentEvent());
             eventBus.post(new DocumentUpdatedEvent());
             leosApplicationEventBus.post(new DocumentUpdatedByCoEditorEvent(user, strDocumentVersionSeriesId, id));
@@ -676,8 +674,6 @@ public class FinancialStatementPresenter extends AbstractLeosPresenter {
             financialStatement = financialStatementService.updateFinancialStatement(financialStatement, updatedXmlContent,
                     VersionType.MINOR, messageHelper.getMessage("operation.financial.block.deleted"));
             if (financialStatement != null) {
-                eventBus.post(new NotificationEvent(NotificationEvent.Type.INFO, "document.financial.block.deleted",
-                        StringUtils.capitalize(tagName)));
                 eventBus.post(new RefreshDocumentEvent());
                 eventBus.post(new DocumentUpdatedEvent());
                 leosApplicationEventBus.post(new DocumentUpdatedByCoEditorEvent(user, strDocumentVersionSeriesId, id));
