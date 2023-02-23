caf6879dee51addf738566e86f41a221311b6a90
adriacr <cristobal.adrian@ext.ec.europa.eu>
2023-02-23 18:21:00 +0100

    LEOSCOMM282#Fixed pass environment values for building angular ui.

--- modules/ui-angular/src/env.d.ts
+++ modules/ui-angular/src/env.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     NG_APP_LEOS_INSTANCE: string;
+    NG_APP_LEOS_SOURCE_REVISION: string;
     NG_APP_LEOS_VERSION: string;
     NG_APP_LEOS_VERSION_BUILD_DATE: string;
   }
