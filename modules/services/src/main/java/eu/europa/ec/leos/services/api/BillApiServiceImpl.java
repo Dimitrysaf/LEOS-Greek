23ac9da4c348f02e497e8b896f87d73d89665639
Stelios Vardakis <s.vardakis@doubleip.com>
2023-03-08 11:06:03 +0100

    #367: Removed service annotation from base document services

--- modules/services/src/main/java/eu/europa/ec/leos/services/api/BillApiServiceImpl.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/api/BillApiServiceImpl.java
@@ -71,7 +71,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-@Service("bill")
 public class BillApiServiceImpl implements BillApiService {
 
     @Autowired
