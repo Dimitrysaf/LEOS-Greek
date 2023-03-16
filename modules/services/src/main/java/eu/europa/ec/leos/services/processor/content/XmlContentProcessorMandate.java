c0d4384ff3daa899dd0428753eebbbbcbb433111
Bhanu Prasad VELLANKI <bhanu-prasad.vellanki@ext.ec.europa.eu>
2023-03-16 08:54:44 +0100

    Revert "Merge branch '336-subparagraph-ignored-when-created' into 'development'"
    This reverts merge request !275

--- modules/services/src/main/java/eu/europa/ec/leos/services/processor/content/XmlContentProcessorMandate.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/processor/content/XmlContentProcessorMandate.java
@@ -292,7 +292,6 @@ public class XmlContentProcessorMandate extends XmlContentProcessorImpl {
                     || tagValue.equals(INDENT)
                     || tagValue.equalsIgnoreCase(CROSSHEADING)
                     || child.isMovedOnEmptyParent()
-                    || content.isEmpty()
                     || (tagValue.equals(LIST)
                     && (!content.isEmpty()  && child.getChildItemsView().size() > 0
                     && getTagValueFromTocItemVo(child.getChildItemsView().get(0)).equals(SUBPARAGRAPH)));
