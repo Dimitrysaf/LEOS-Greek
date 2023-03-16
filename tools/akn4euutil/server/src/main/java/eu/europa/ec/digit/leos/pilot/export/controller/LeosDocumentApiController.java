de6e0d3b7ccb31c96e7b61009a88566a39514418
adriacr <cristobal.adrian@ext.ec.europa.eu>
2023-03-16 11:26:28 +0100

    CORE#419: Enable CORS for rest services.

--- tools/akn4euutil/server/src/main/java/eu/europa/ec/digit/leos/pilot/export/controller/LeosDocumentApiController.java
+++ tools/akn4euutil/server/src/main/java/eu/europa/ec/digit/leos/pilot/export/controller/LeosDocumentApiController.java
@@ -19,6 +19,7 @@ import eu.europa.ec.digit.leos.pilot.export.service.LeosDocumentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,6 +31,7 @@ import static eu.europa.ec.digit.leos.pilot.export.util.DocumentApiUtil.buildErr
 import static eu.europa.ec.digit.leos.pilot.export.util.DocumentApiUtil.buildValidZipResponse;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class LeosDocumentApiController {
 
     private final LeosDocumentService leosDocumentService;
