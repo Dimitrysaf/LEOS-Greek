28d238b86109439e759e496c63565aa52e54ed3f
Stelios Vardakis <s.vardakis@doubleip.com>
2023-03-29 15:46:33 +0300

    added websocket impl only for testing purposes

---
+++ modules/services/src/main/java/eu/europa/ec/leos/services/config/CoEditionWebSocketConfig.java
@@ -0,0 +1,34 @@
+package eu.europa.ec.leos.services.config;
+
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class CoEditionWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //TODO : allow all origins with * for development phase later will be removed
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+}
---
+++ modules/services/src/main/java/eu/europa/ec/leos/services/controllers/CoEditionController.java
@@ -0,0 +1,26 @@
+package eu.europa.ec.leos.services.controllers;
+
+import com.github.jsonldjava.utils.Obj;
+import eu.europa.ec.leos.services.dto.request.HelloMessage;
+import eu.europa.ec.leos.services.dto.response.Greeting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class CoEditionController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CoEditionController.class);
+
+    //this is a small demo with the only purpose is to test if there is extra needed for configuration on the tomcat production server
+    @MessageMapping("/hello")
+    @SendTo("/topic/greeting")
+    public Greeting greeting(HelloMessage message) {
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+
+}
