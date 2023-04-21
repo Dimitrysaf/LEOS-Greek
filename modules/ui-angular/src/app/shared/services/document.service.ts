2920f0626d77d374dbee6b048576a0cb1056a1c3
Stelios Vardakis <s.vardakis@doubleip.com>
2023-04-21 11:08:52 +0200

    core #541 - "Cover page - cannot be opened"

--- modules/ui-angular/src/app/shared/services/document.service.ts
+++ modules/ui-angular/src/app/shared/services/document.service.ts
@@ -729,8 +729,10 @@ export class DocumentService implements OnDestroy {
   }
 
   private getDocumentConfig(documentRef: string, documentType: string) {
+    let category = this.documentCategoryBS.value;
+    category = category === 'coverpage' ? 'coverPage' : category;
     return this.http.get<DocumentConfig>(
-      `${apiBaseUrl}/secured/${documentType}/${documentRef}/document-config`,
+      `${apiBaseUrl}/secured/${category}/${documentRef}/document-config`,
     );
   }
 
@@ -905,6 +907,7 @@ export class DocumentService implements OnDestroy {
   }
 
   private getCollaborators(proposalRef: string) {
+    if (!proposalRef) return;
     return this.http.get<Collaborator[]>(
       `${apiBaseUrl}/secured/proposal/${proposalRef}/collaborators`,
     );
