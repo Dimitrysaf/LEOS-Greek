fd3b35ed6a492abd6c072adcbc6701c7a83dab17
Stelios Vardakis <s.vardakis@doubleip.com>
2023-05-03 14:43:27 +0200

    core #632 - invalid message when importing from official journal"

--- modules/services/src/main/java/eu/europa/ec/leos/services/api/BillApiServiceImpl.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/api/BillApiServiceImpl.java
@@ -383,7 +383,8 @@ public class BillApiServiceImpl implements BillApiService {
                     metadata.getLanguage());
             String notificationMsg = "document.import.element.inserted" + (elementIds.stream().anyMatch((s) -> s.startsWith("rec_")) ? ".recitals" : "") +
                     (elementIds.stream().anyMatch((s) -> s.startsWith("art_")) ? ".articles" : "");
-            bill = billService.updateBill(bill, newXmlContent, messageHelper.getMessage(notificationMsg));
+            String operationMessage = messageHelper.getMessage("operation.import.element.inserted");
+            bill = billService.updateBill(bill, newXmlContent, operationMessage);
             return this.documentViewService.getDocumentView(bill);
         } else {
             throw new ImportElementException("Search returned with no result! Please modify the search parameters");
