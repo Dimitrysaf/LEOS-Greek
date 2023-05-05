a5b5b0d93968c0fca180bb88a94ef5f2e666cb30
Stelios Vardakis <s.vardakis@doubleip.com>
2023-05-05 16:39:03 +0300

    core #690 - fixed wrong encoded

--- modules/services/src/main/java/eu/europa/ec/leos/services/controllers/MemorandumController.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/controllers/MemorandumController.java
@@ -242,7 +242,7 @@ public class MemorandumController {
 
     }
 
-    @GetMapping(value = "/{newVersionId}/compare/{oldVersionId}", produces = MediaType.TEXT_HTML_VALUE)
+    @GetMapping(value = "/{newVersionId}/compare/{oldVersionId}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<Object> compareMemorandumVersions(@PathVariable("newVersionId") String newVersionId,
                                                             @PathVariable("oldVersionId") String oldVersionId) {
@@ -381,7 +381,7 @@ public class MemorandumController {
             final String jobFileName = documentRef + "_AKN2DW_CLEAN_" + System.currentTimeMillis() + ".docx";
             // create the HttpHeaders object and set the Content-Type header
             HttpHeaders headers = new HttpHeaders();
-            headers.set("Content-Disposition", "attachment; filename="+jobFileName);
+            headers.set("Content-Disposition", "attachment; filename=" + jobFileName);
             return new ResponseEntity<>(cleanVersion, headers, HttpStatus.OK);
         } catch (Exception e) {
             LOG.error("Error occurred  while trying to download clean version for memorandum " + e.getMessage());
