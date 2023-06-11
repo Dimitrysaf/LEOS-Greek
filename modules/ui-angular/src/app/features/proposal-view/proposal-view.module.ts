7fbbab9f6cac9031f9a3981a6364fc8d3cae224b
Nikos Mariolis <ngmariolis@gmail.com>
2023-06-11 22:06:45 +0300

    Core #792: Created warning modal for contribution proposal

--- modules/ui-angular/src/app/features/proposal-view/proposal-view.module.ts
+++ modules/ui-angular/src/app/features/proposal-view/proposal-view.module.ts
@@ -15,6 +15,7 @@ import { ProposalMilestoneSendCopyDialogComponent } from './containers/proposal-
 import { ProposalMilestonesComponent } from './containers/proposal-milestones/proposal-milestones.component';
 import { ProposalViewComponent } from './containers/proposal-view/proposal-view.component';
 import { ProposalViewRoutingModule } from './proposal-view-routing.module';
+import { MilestoneAnnotationWarningModalComponent } from './components/milestone-annotation-warning-modal/milestone-annotation-warning-modal.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { ProposalViewRoutingModule } from './proposal-view-routing.module';
     ProposalExportsComponent,
     AddMilestoneDialogComponent,
     ProposalMilestoneSendCopyDialogComponent,
+    MilestoneAnnotationWarningModalComponent,
   ],
   providers: [ProposalService],
   imports: [ProposalViewRoutingModule, SharedModule],
