84d28cbf063ff17f1428132c03e6a0455342b19b
Nikos Mariolis <ngmariolis@gmail.com>
2023-06-11 22:06:45 +0300

    Core #792: Created warning modal for contribution proposal

---
+++ modules/ui-angular/src/app/features/proposal-view/components/milestone-annotation-warning-modal/milestone-annotation-warning-modal.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MilestoneAnnotationWarningModalComponent } from './milestone-annotation-warning-modal.component';
+
+describe('MilestoneAnnotationWarningModalComponent', () => {
+  let component: MilestoneAnnotationWarningModalComponent;
+  let fixture: ComponentFixture<MilestoneAnnotationWarningModalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MilestoneAnnotationWarningModalComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MilestoneAnnotationWarningModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ modules/ui-angular/src/app/features/proposal-view/components/milestone-annotation-warning-modal/milestone-annotation-warning-modal.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-milestone-annotation-warning-modal',
+  templateUrl: './milestone-annotation-warning-modal.component.html',
+  styleUrls: ['./milestone-annotation-warning-modal.component.scss']
+})
+export class MilestoneAnnotationWarningModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAccept(){
+
+  }
+
+  onClose(){
+
+  }
+}
