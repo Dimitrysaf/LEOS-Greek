ed2f5a7ebd3a9acd5e11d19532a35630fa16764e
Nikos Mariolis <ngmariolis@gmail.com>
2023-06-12 23:04:33 +0300

    Core #792: annotation warning modal completed. Await apis to continue.

--- modules/ui-angular/src/app/features/proposal-view/components/milestone-annotation-warning-modal/milestone-annotation-warning-modal.component.ts
+++ modules/ui-angular/src/app/features/proposal-view/components/milestone-annotation-warning-modal/milestone-annotation-warning-modal.component.ts
@@ -1,22 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { EuiDialogComponent } from '@eui/components/eui-dialog/eui-dialog.component';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-milestone-annotation-warning-modal',
   templateUrl: './milestone-annotation-warning-modal.component.html',
-  styleUrls: ['./milestone-annotation-warning-modal.component.scss']
+  styleUrls: ['./milestone-annotation-warning-modal.component.scss'],
 })
-export class MilestoneAnnotationWarningModalComponent implements OnInit {
+export class MilestoneAnnotationWarningModalComponent
+  implements OnInit, OnDestroy
+{
+  @Output() closed = new EventEmitter();
+  @Output() modalAccepted = new EventEmitter();
+  destroy$ = new Subject<any>();
+  @ViewChild('milestoneAnnotationWarningModal')
+  milestoneAnnotationWarningModal: EuiDialogComponent;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  ngOnDestroy() {
+    this.destroy$.next(null);
+    this.destroy$.complete();
   }
 
-  onAccept(){
-
+  open() {
+    this.milestoneAnnotationWarningModal.openDialog();
   }
 
-  onClose(){
-
+  close() {
+    this.milestoneAnnotationWarningModal.closeDialog();
+    this.closed.emit();
   }
+
+  onAccept() {
+    this.modalAccepted.emit();
+    this.close();
+  }
+
+  onClose() {}
 }
