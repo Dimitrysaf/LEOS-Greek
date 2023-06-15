dc068e193615667e2d32ff6658396a051550d79d
Nikos Mariolis <ngmariolis@gmail.com>
2023-06-15 11:03:06 +0300

    Core #670: created directives for conditional display

---
+++ modules/ui-angular/src/app/shared/directives/is-recieved-for-contribution.directive.ts
@@ -0,0 +1,44 @@
+import {
+  Directive,
+  Input,
+  OnDestroy,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
+import { Subject } from 'rxjs';
+
+import { AppConfigService } from '@/core/services/app-config.service';
+import { Document } from '@/shared';
+
+@Directive({
+  selector: '[appIsRecievedForContribution]',
+})
+export class IsRecievedForContributionDirective implements OnDestroy {
+  private destroy$: Subject<any> = new Subject();
+  private userLogin: string;
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainerRef: ViewContainerRef,
+    private appConfig: AppConfigService,
+  ) {
+    this.appConfig.config.pipe().subscribe((config) => {
+      this.userLogin = config.user.login;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(null);
+    this.destroy$.complete();
+  }
+
+  @Input() set appIsRecievedForContribution(proposal: Document) {
+    if (
+      proposal.cloneProposalMetadataVO.clonedProposal &&
+      proposal.cloneProposalMetadataVO.targetUser !== this.userLogin
+    ) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainerRef.clear();
+    }
+  }
+}
