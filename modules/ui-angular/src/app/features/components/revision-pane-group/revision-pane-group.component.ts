17e0c35c711b6cef71772bf6082f3cabdc921f09
Nikos Mariolis <ngmariolis@gmail.com>
2023-06-19 19:02:19 +0300

    Core #798: created new components and translations

---
+++ modules/ui-angular/src/app/features/components/revision-pane-group/revision-pane-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-revision-pane-group',
+  templateUrl: './revision-pane-group.component.html',
+  styleUrls: ['./revision-pane-group.component.scss']
+})
+export class RevisionPaneGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
