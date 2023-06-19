b1927afec3f0ada9a547bfcf2122a5220176faab
Nikos Mariolis <ngmariolis@gmail.com>
2023-06-19 21:43:36 +0300

    Core #798: work on api implementation

---
+++ modules/ui-angular/src/app/shared/models/contribution-vo.model.ts
@@ -0,0 +1,16 @@
+import { CheckinComment } from '@/features/akn-document/models';
+import { Collaborator } from '@/shared';
+
+export interface ContributionVO {
+  documentId: string;
+  versionNumber: number[];
+  updatedDate: number;
+  versionedReference: string;
+  checkinCommentVO: CheckinComment;
+  contributionCreator: string;
+  contributionStatus: string;
+  collaborators: Collaborator[];
+  xmlContent: any;
+  legFileName: string;
+  documentName: string;
+}
