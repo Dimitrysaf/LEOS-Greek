c5bc16c30de529d8513594df239c0c31d5f99bda
vellabh <bhanu-prasad.vellanki@ext.ec.europa.eu>
2023-06-21 20:56:18 +0200

    Core #847 - Fix for issue with akn4eu version upgrade.

--- modules/services/src/main/java/eu/europa/ec/leos/services/processor/node/XmlNodeProcessorImpl.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/processor/node/XmlNodeProcessorImpl.java
@@ -228,7 +228,7 @@ public class XmlNodeProcessorImpl implements XmlNodeProcessor {
                 content = value;
             }
             // 3. Inject newly created node in XML
-            Node newNode = XercesUtils.createElementWithAknNS(document, tagName, content);
+            Node newNode = XercesUtils.createElement(document, tagName, content);
             node = XercesUtils.addChild(newNode, node);
             for (Attribute attr : attributes) {
                 XercesUtils.addAttribute(node, attr.name, attr.value);
