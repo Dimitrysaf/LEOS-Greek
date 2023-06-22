0e21037dcf3b8b69566819aa130143979e087628
Polizois N <n.polyzois@doubleip.com>
2023-06-22 18:31:46 +0300

    [820] - add contribution status tooltip functionality

--- modules/ui-angular/src/app/features/akn-document/components/revision-pane-group/revision-pane-group.component.ts
+++ modules/ui-angular/src/app/features/akn-document/components/revision-pane-group/revision-pane-group.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import * as cluster from 'cluster';
+import { TranslateService } from '@ngx-translate/core';
 
-import { Version } from '@/features/akn-document/models';
 import { ContributionVO } from '@/shared/models/contribution-vo.model';
 
 @Component({
@@ -18,11 +17,12 @@ export class RevisionPaneGroupComponent implements OnInit {
   updatedAtBy: string;
   status: string;
 
-  constructor() {}
+  constructor(private translate: TranslateService) {}
 
   ngOnInit(): void {
     this.revisionTitle = this.formatTitle(this.contribution);
     this.revisionVersion = this.formatVersionNumber(this.contribution);
+    this.status = this.contributionStatusTooltipText(this.contribution);
   }
 
   protected formatVersionNumber(contribution: ContributionVO): string {
@@ -39,5 +39,17 @@ export class RevisionPaneGroupComponent implements OnInit {
     } else {
       return contribution.checkinCommentVO.title;
     }
+  }
+
+  protected contributionStatusTooltipText(
+    contribution: ContributionVO,
+  ): string {
+    return contribution.contributionStatus === 'RECEIVED'
+      ? this.translate.instant(
+          'page.workspace.proposal-item.contribution-received',
+        )
+      : this.translate.instant(
+          'page.workspace.proposal-item.contribution-done',
+        );
   }
 }
