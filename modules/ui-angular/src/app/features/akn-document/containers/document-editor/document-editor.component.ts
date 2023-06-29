2c3a606c1bb951510f937518a992c77c0fd662d2
Nikos Mariolis <ngmariolis@gmail.com>
2023-06-29 20:53:21 +0300

    Core #801: removed contribution annotations broken implementation

--- modules/ui-angular/src/app/features/akn-document/containers/document-editor/document-editor.component.ts
+++ modules/ui-angular/src/app/features/akn-document/containers/document-editor/document-editor.component.ts
@@ -92,14 +92,7 @@ export class DocumentEditorComponent
   isVersionsPaneCollapsed = true;
   isContributionForViewOpen = false;
   isViewContributionPaneCollapsed = true;
-  isContributionAnnotationPaneCollapsed = true;
   reloadTrigger: number;
-
-  contributionDocumentRef: string;
-  contributionContainerId: 'contributionViewContainer';
-  contributionsConnectedEntity: string;
-  contributionProposalRef: string;
-  contributionShowStatusFilter: boolean;
 
   tocItems: Array<TocItem> = [];
   dragItems: Array<Partial<TableOfContentItemVO>> = [];
@@ -220,7 +213,6 @@ export class DocumentEditorComponent
           config.user.connectedEntity ?? config.user.defaultEntity
         ).name;
         this.showStatusFilter = config.annotateAuthority === 'LEOS';
-        this.contributionShowStatusFilter = config.annotateAuthority === 'LEOS';
         this.documentService.setDocumentRefAndCategory(
           this.documentRef,
           this.documentType,
@@ -264,9 +256,6 @@ export class DocumentEditorComponent
           this.contributionForView = this.cleanupAndSerializeXML(
             contributionView.editableXml,
           );
-          this.contributionsConnectedEntity = this.connectedEntity;
-          this.contributionDocumentRef = this.documentRef;
-          this.contributionProposalRef = contributionView.proposalRef;
           this.isContributionForViewOpen = true;
           this.isViewContributionPaneCollapsed = false;
         }
@@ -473,20 +462,6 @@ export class DocumentEditorComponent
     hideAnnotationsSplitter = !this.hideAnnotationsSplitter,
   ) {
     this.hideAnnotationsSplitter = hideAnnotationsSplitter;
-  }
-
-  onToggleContributionAnnotationsPaneCollapsed(
-    isContributionAnnotationPaneCollapsed = !this
-      .isContributionAnnotationPaneCollapsed,
-  ) {
-    (
-      document.querySelector(
-        'button.annotator-frame-button--sidebar_toggle',
-      ) as HTMLButtonElement
-    )?.click();
-    this.isContributionAnnotationPaneCollapsed =
-      isContributionAnnotationPaneCollapsed;
-    // if (!this.isContributionAnnotationPaneCollapsed) this.onHideAnnotationsSplitter(false);
   }
 
   handleEdit() {
@@ -735,12 +710,6 @@ export class DocumentEditorComponent
   onSidebarShown() {
     if (this.isAnnotationsPaneCollapsed) {
       this.onToggleAnnotationsPaneCollapsed();
-    }
-  }
-
-  onContributionSidebarShown() {
-    if (this.isContributionAnnotationPaneCollapsed) {
-      this.onToggleContributionAnnotationsPaneCollapsed();
     }
   }
 
