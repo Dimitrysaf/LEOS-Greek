128ee20815c68e63a6151e970560dbea463ac773
Panos Kalodimas <pkalodimas@gmail.com>
2023-06-29 13:45:22 +0300

    Core #820: Reset leosArticleListPlugin.js

--- modules/js/src/main/js/editor/plugins/leosArticleList/leosArticleListPlugin.js
+++ modules/js/src/main/js/editor/plugins/leosArticleList/leosArticleListPlugin.js
@@ -170,13 +170,13 @@ define(function leosArticleListPluginModule(require) {
                         currentListItem.append( item.contents[ i ].clone( 1, 1 ) );
                     currentIndex++;
                 } else if ( item.indent == Math.max( indentLevel, 0 ) + 1 ) {
-                    // Maintain original direction (http://dev.ckeditor.com/ticket/6861).
+					// Maintain original direction (http://dev.ckeditor.com/ticket/6861).
                     var currDir = listArray[ currentIndex - 1 ].element.getDirection( 1 ),
                         listData = CKEDITOR.plugins.leosArticleList.arrayToList( listArray, null, currentIndex, paragraphMode, currDir != orgDir ? orgDir : null );
 
                     // If the next block is an <li> with another list tree as the first
                     // child, we'll need to append a filler (<br>/NBSP) or the list item
-                    // wouldn't be editable. (http://dev.ckeditor.com/ticket/6724)
+					// wouldn't be editable. (http://dev.ckeditor.com/ticket/6724)
                     if ( !currentListItem.getChildCount() && CKEDITOR.env.needsNbspFiller && doc.$.documentMode <= 7 )
                         currentListItem.append( doc.createText( '\xa0' ) );
                     currentListItem.append( listData.listNode );
@@ -510,7 +510,7 @@ define(function leosArticleListPluginModule(require) {
 
         var newList = CKEDITOR.plugins.leosArticleList.arrayToList( listArray, database, null, editor.config.enterMode, groupObj.root.getAttribute( 'dir' ) );
 
-        // Compensate <br> before/after the list node if the surrounds are non-blocks.(http://dev.ckeditor.com/ticket/3836)
+		// Compensate <br> before/after the list node if the surrounds are non-blocks.(http://dev.ckeditor.com/ticket/3836)
         var docFragment = newList.listNode,
             boundaryNode, siblingNode;
 
@@ -537,9 +537,9 @@ define(function leosArticleListPluginModule(require) {
     // Checks wheather this block should be element preserved (not transformed to <li>) when creating list.
     function shouldPreserveBlock( block ) {
         return (
-            // http://dev.ckeditor.com/ticket/5335
+			// http://dev.ckeditor.com/ticket/5335
             block.is( 'pre' ) ||
-            // http://dev.ckeditor.com/ticket/5271 - this is a header.
+			// http://dev.ckeditor.com/ticket/5271 - this is a header.
             headerTagRegex.test( block.getName() ) ||
             // 11083 - this is a non-editable element.
             block.getAttribute( 'contenteditable' ) == 'false'
@@ -556,7 +556,7 @@ define(function leosArticleListPluginModule(require) {
 
     var elementType = CKEDITOR.dom.walker.nodeType( CKEDITOR.NODE_ELEMENT );
 
-    // Merge child nodes with direction preserved. (http://dev.ckeditor.com/ticket/7448)
+	// Merge child nodes with direction preserved. (http://dev.ckeditor.com/ticket/7448)
     function mergeChildren( from, into, refNode, forward ) {
         var child, itemDir;
         while ( ( child = from[ forward ? 'getLast' : 'getFirst' ]( elementType ) ) ) {
@@ -590,7 +590,7 @@ define(function leosArticleListPluginModule(require) {
                     selection.selectRanges( ranges );
                 }
                 // Maybe a single range there enclosing the whole list,
-                // turn on the list state manually(http://dev.ckeditor.com/ticket/4129).
+				// turn on the list state manually(http://dev.ckeditor.com/ticket/4129).
                 else {
                     var range = ranges.length == 1 && ranges[ 0 ],
                         enclosedNode = range && range.getEnclosedNode();
@@ -640,13 +640,13 @@ define(function leosArticleListPluginModule(require) {
 
                     // First, try to group by a list ancestor.
                     for ( var i = pathElementsCount - 1; i >= 0 && ( element = pathElements[ i ] ); i-- ) {
-                        // Don't leak outside block limit (http://dev.ckeditor.com/ticket/3940).
+						// Don't leak outside block limit (http://dev.ckeditor.com/ticket/3940).
                         if ( listNodeNames[ element.getName() ] && blockLimit.contains( element ) ) {
                             // If we've encountered a list inside a block limit
                             // The last group object of the block limit element should
                             // no longer be valid. Since paragraphs after the list
                             // should belong to a different group of paragraphs before
-                            // the list. (Bug http://dev.ckeditor.com/ticket/1309)
+							// the list. (Bug http://dev.ckeditor.com/ticket/1309)
                             blockLimit.removeCustomData( 'list_group_object_' + index );
 
                             var groupObj = element.getCustomData( 'list_group_object' );
@@ -751,7 +751,7 @@ define(function leosArticleListPluginModule(require) {
         function mergeSibling( rtl ) {
             var sibling = listNode[ rtl ? 'getPrevious' : 'getNext' ]( nonEmpty );
             if ( sibling && sibling.type == CKEDITOR.NODE_ELEMENT && sibling.is( listNode.getName() ) ) {
-                // Move children order by merge direction.(http://dev.ckeditor.com/ticket/3820)
+				// Move children order by merge direction.(http://dev.ckeditor.com/ticket/3820)
                 mergeChildren( listNode, sibling, null, !rtl );
 
                 listNode.remove();
@@ -852,7 +852,7 @@ define(function leosArticleListPluginModule(require) {
             nextPath = nextCursor.startPath();
             nextBlock = nextPath.block;
 
-            // Abort when nothing to be removed (http://dev.ckeditor.com/ticket/10890).
+			// Abort when nothing to be removed (http://dev.ckeditor.com/ticket/10890).
             if ( !nextBlock )
                 break;
 
@@ -956,7 +956,7 @@ define(function leosArticleListPluginModule(require) {
             // Register the toolbar button.
             // DELETED!
 
-            // Handled backspace/del key to join list items. (http://dev.ckeditor.com/ticket/8248,http://dev.ckeditor.com/ticket/9080)
+			// Handled backspace/del key to join list items. (http://dev.ckeditor.com/ticket/8248,http://dev.ckeditor.com/ticket/9080)
             editor.on( 'key', function( evt ) {
                 // Use getKey directly in order to ignore modifiers.
                 // Justification: http://dev.ckeditor.com/ticket/11861#comment:13
@@ -1025,7 +1025,7 @@ define(function leosArticleListPluginModule(require) {
                                 // Place cursor at the end of previous block.
                                 cursor.moveToElementEditEnd( joinWith );
 
-                                // And then just before end of closest block element (http://dev.ckeditor.com/ticket/12729).
+								// And then just before end of closest block element (http://dev.ckeditor.com/ticket/12729).
                                 cursor.moveToPosition( cursor.endPath().block, CKEDITOR.POSITION_BEFORE_END );
                             }
                         }
@@ -1048,7 +1048,7 @@ define(function leosArticleListPluginModule(require) {
                         }
                         else {
                             var list = path.contains( listNodeNames );
-                            // Backspace pressed at the start of list outdents the first list item. (http://dev.ckeditor.com/ticket/9129)
+							// Backspace pressed at the start of list outdents the first list item. (http://dev.ckeditor.com/ticket/9129)
                             if ( list && range.checkBoundaryOfElement( list, CKEDITOR.START ) ) {
                                 li = list.getFirst( nonEmpty );
 
@@ -1110,7 +1110,7 @@ define(function leosArticleListPluginModule(require) {
                                 nextLine = range.clone();
                                 nextLine.moveToElementEditStart( next );
 
-                                // http://dev.ckeditor.com/ticket/13409
+								// http://dev.ckeditor.com/ticket/13409
                                 // For the following case and similar
                                 //
                                 // <ul>
@@ -1178,7 +1178,7 @@ define(function leosArticleListPluginModule(require) {
                                     }
                                 }
 
-                                // Moving `cursor` and `next line` only when at the end literally (http://dev.ckeditor.com/ticket/12729).
+								// Moving `cursor` and `next line` only when at the end literally (http://dev.ckeditor.com/ticket/12729).
                                 if ( isAtEnd == 2 ) {
                                     cursor.moveToPosition( cursor.endPath().block, CKEDITOR.POSITION_BEFORE_END );
 
