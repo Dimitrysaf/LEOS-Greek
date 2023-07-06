54bddae1271cf28ff0104af414f36c5eb0402773
nunejoa <Joao-Diogo.NUNES-CARTAXO@yumprd.tech.ec.europa.eu>
2023-07-06 14:12:22 +0200

    core#917 - SonarQube issues fix

--- modules/i18n/src/main/java/eu/europa/ec/leos/i18n/MessageHelper.java
+++ modules/i18n/src/main/java/eu/europa/ec/leos/i18n/MessageHelper.java
@@ -13,8 +13,6 @@
  */
 package eu.europa.ec.leos.i18n;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.MessageSource;
