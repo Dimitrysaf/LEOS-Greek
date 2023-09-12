51bda678f529eda02615cfcc3e521aaccf043c17
Pascal NYSTEN <NYSTEPA>
2023-09-12 15:50:09 +0200

    972 correction null pointer

--- tools/repository/server/src/etc/database/mysql/1_env_setup.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER 'leos_repository'@'localhost' IDENTIFIED BY 'leos_repository';
-GRANT ALL PRIVILEGES ON *.* TO 'leos_repository'@'localhost';
-CREATE DATABASE IF NOT EXISTS leos_repository;
-CREATE TABLESPACE leos_repository ADD DATAFILE 'leos_repository.ibd' ENGINE = INNODB;
