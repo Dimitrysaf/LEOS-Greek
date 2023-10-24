5a3cb7bf938dead9618839ce90db132154d8a1be
Ermal ALIRAJ <Ermal.ALIRAJ@ext.ec.europa.eu>
2023-10-24 09:38:46 +0200

    #1129 Track Changes : Associate Accept and Reject with role permissions in contribution panel

--- modules/js/src/main/js/editor/plugins/leosTrackChanges/leosTrackChanges.js
+++ modules/js/src/main/js/editor/plugins/leosTrackChanges/leosTrackChanges.js
@@ -339,7 +339,7 @@ define(function leosTrackChangesModule(require) {
         },
 
         canUserAcceptChanges: function(editor) {
-            return !editor.LEOS.isClonedProposal && editor.LEOS.user.permissions && editor.LEOS.user.permissions.includes("CAN_ACCEPT_CHANGES") )
+            return !editor.LEOS.isClonedProposal && editor.LEOS.user.permissions && editor.LEOS.user.permissions.includes("CAN_ACCEPT_CHANGES")
         },
 
         canUserRejectChanges: function(editor) {
