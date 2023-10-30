c0c7e0de9d41219e2dc059984b3e715e9f3c8053
Stelios Vardakis <s.vardakis@doubleip.com>
2023-10-30 13:46:16 +0100

    core #1010 "Previous change and next change synchronization is not working properly"

--- modules/ui-angular/src/app/features/akn-document/akn-document.module.ts
+++ modules/ui-angular/src/app/features/akn-document/akn-document.module.ts
@@ -38,64 +38,66 @@ import { TableOfContentEditService } from './services/table-of-content-edit.serv
 import { ValidateTocMandateService } from './services/validate-node-drop.mandate.service';
 import { ValidateTocProposalService } from './services/validate-node-drop.proposal.service';
 import { ValidateTocService } from './services/validate-node-drop.service';
+import { SyncDocumentScrollService } from './services/sync-document-scroll.service';
 import {TrackChangesActionsService} from "@/features/akn-document/services/track-changes-actions.service";
 import {DocumentService} from "@/shared/services/document.service";
 
 @NgModule({
-    declarations: [
-        AnnexActionsDropdownComponent,
-        AnnexDocumentComponent,
-        ActionsToolbarComponent,
-        DocumentSearchComponent,
-        VersionActionsDropdownComponent,
-        VersionsPaneComponent,
-        VersionsPaneGroupComponent,
-        DocumentEditorComponent,
-        DownloadEconsiliumModalComponent,
-        ImportFromJournalDialogComponent,
-        TocActionsButtonsComponent,
-        TocEditorComponent,
-        DocumentTocComponent,
-        NodeTocActionsComponent,
-        SaveVersionDialogComponent,
-        RevisionPaneComponent,
-        RevisionPaneGroupComponent,
-        RevisionActionsDropdownComponent,
-        TrackChangesActionsComponent,
-    ],
-    imports: [
-        CommonModule,
-        SharedModule,
-        ReactiveFormsModule,
-        AknDocumentRoutingModule,
-        LeosLegacyModule,
-        SharedModule,
-        MatTreeModule,
-        MatTooltipModule,
-    ],
-    providers: [
-        CKEditorService,
-        {provide: RouteReuseStrategy, useClass: AknRouteReUseStrategy},
-        {
-            provide: ValidateTocService,
-            useClass:
-                process.env.NG_APP_LEOS_INSTANCE.toLowerCase() === CN.toLowerCase()
-                    ? ValidateTocMandateService
-                    : ValidateTocProposalService,
-        },
-        {
-            provide: TableOfContentEditService,
-            useClass:
-                process.env.NG_APP_LEOS_INSTANCE.toLowerCase() === CN.toLowerCase()
-                    ? TableOfContentMandateEditService
-                    : TableOfContentProposalEditService,
-        },
-        BlockDocumentEditorService,
-        DocumentService,
-        TrackChangesActionsService,
-    ],
-    exports: [
-        TrackChangesActionsComponent
-    ]
+  declarations: [
+    AnnexActionsDropdownComponent,
+    AnnexDocumentComponent,
+    ActionsToolbarComponent,
+    DocumentSearchComponent,
+    VersionActionsDropdownComponent,
+    VersionsPaneComponent,
+    VersionsPaneGroupComponent,
+    DocumentEditorComponent,
+    DownloadEconsiliumModalComponent,
+    ImportFromJournalDialogComponent,
+    TocActionsButtonsComponent,
+    TocEditorComponent,
+    DocumentTocComponent,
+    NodeTocActionsComponent,
+    SaveVersionDialogComponent,
+    RevisionPaneComponent,
+    RevisionPaneGroupComponent,
+    RevisionActionsDropdownComponent,
+    TrackChangesActionsComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    ReactiveFormsModule,
+    AknDocumentRoutingModule,
+    LeosLegacyModule,
+    SharedModule,
+    MatTreeModule,
+    MatTooltipModule,
+  ],
+  providers: [
+    CKEditorService,
+    SyncDocumentScrollService,
+    { provide: RouteReuseStrategy, useClass: AknRouteReUseStrategy },
+    {
+      provide: ValidateTocService,
+      useClass:
+        process.env.NG_APP_LEOS_INSTANCE.toLowerCase() === CN.toLowerCase()
+          ? ValidateTocMandateService
+          : ValidateTocProposalService,
+    },
+    {
+      provide: TableOfContentEditService,
+      useClass:
+        process.env.NG_APP_LEOS_INSTANCE.toLowerCase() === CN.toLowerCase()
+          ? TableOfContentMandateEditService
+          : TableOfContentProposalEditService,
+    },
+    BlockDocumentEditorService,
+    DocumentService,
+    TrackChangesActionsService,
+  ],
+  exports: [
+    TrackChangesActionsComponent
+  ]
 })
 export class AknDocumentModule {}
