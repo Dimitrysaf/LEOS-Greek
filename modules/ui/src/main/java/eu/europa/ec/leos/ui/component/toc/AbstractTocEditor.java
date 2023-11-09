bc61d1b289480415f9f12f4f40595e87301bd9a9
Pascal NYSTEN <NYSTEPA>
2023-11-09 16:38:45 +0100

    1179 TOC Delete on COUNCIL

--- modules/ui/src/main/java/eu/europa/ec/leos/ui/component/toc/AbstractTocEditor.java
+++ modules/ui/src/main/java/eu/europa/ec/leos/ui/component/toc/AbstractTocEditor.java
@@ -823,7 +823,7 @@ public abstract class AbstractTocEditor implements TocEditor {
     private TableOfContentItemVO copyDeletedItemToTemp(TableOfContentItemVO originalItem, Boolean isSoftActionRoot) {
         TableOfContentItemVO tempDeletedItem;
 
-        if (!MOVE_TO.equals(originalItem.getSoftActionAttr()) && !isDeletedItem(originalItem)) {
+        if (!MOVE_TO.equals(originalItem.getSoftActionAttr()) && !DELETE.equals(originalItem.getSoftActionAttr())) {
             tempDeletedItem = new TableOfContentItemVO(originalItem.getTocItem(), TEMP_PREFIX + SOFT_DELETE_PLACEHOLDER_ID_PREFIX + originalItem.getId(),
                     originalItem.getOriginAttr(), originalItem.getNumber(),
                     EC, originalItem.getHeading(), originalItem.getNode(),
