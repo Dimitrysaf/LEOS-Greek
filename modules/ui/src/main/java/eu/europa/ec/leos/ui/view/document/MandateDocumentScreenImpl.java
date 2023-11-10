6b03ee02c62605df5d848d9d961e3acc08146f59
Pascal NYSTEN <NYSTEPA>
2023-11-10 10:41:03 +0100

    1179 TOC Delete on COUNCIL

--- modules/ui/src/main/java/eu/europa/ec/leos/ui/view/document/MandateDocumentScreenImpl.java
+++ modules/ui/src/main/java/eu/europa/ec/leos/ui/view/document/MandateDocumentScreenImpl.java
@@ -123,7 +123,7 @@ public class MandateDocumentScreenImpl extends DocumentScreenImpl {
     @Override
     public void refreshContent(final String documentContent, boolean trackChangesEnabled) {
         eventBus.post(new AddChangeDetailsMenuEvent());
-        legalTextPaneComponent.populateContent(documentContent);
+        legalTextPaneComponent.populateContent(documentContent, trackChangesEnabled);
     }
 
     @Override
