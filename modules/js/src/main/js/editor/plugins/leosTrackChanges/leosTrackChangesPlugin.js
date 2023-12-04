7d2be475387d1bfc22e8c41c72104b800d005bc1
GUPTANI <nikhil.gupta@ext.ec.europa.eu>
2023-12-04 12:42:18 +0100

    core#1271 revert TC on original proposal

--- modules/js/src/main/js/editor/plugins/leosTrackChanges/leosTrackChangesPlugin.js
+++ modules/js/src/main/js/editor/plugins/leosTrackChanges/leosTrackChangesPlugin.js
@@ -29,6 +29,9 @@ define(function leosTrackChangesPluginModule(require) {
     var pluginDefinition = {
         init: function init(editor) {
             // Plugin only allowed for cloned proposals
+            if (!editor.LEOS.isClonedProposal) {
+                return;
+            }
 
             var core = trackChanges.core, actions = trackChanges.actions, style = trackChangesStyle.style, table = trackChangesTable.table;
             var isTrackChangesShowed = editor.LEOS.isTrackChangesShowed, isTrackChangesEnabled = editor.LEOS.isTrackChangesEnabled;
--- modules/repository/src/main/java/eu/europa/ec/leos/cmis/repository/LeosCmisRepositoryImpl.java
+++ modules/repository/src/main/java/eu/europa/ec/leos/cmis/repository/LeosCmisRepositoryImpl.java
@@ -112,7 +112,6 @@ public class LeosCmisRepositoryImpl implements LeosRepository {
         long startTimeNanos = System.nanoTime();
         Map<String, Object> properties = new HashMap<>();
         properties.put(PropertyIds.NAME, name);
-        properties.put(repositoryPropertiesMapper.getId(RepositoryProperties.TRACK_CHANGES_ENABLED), true);
         setDocumentCollaboratorProperties(metadata, properties);
 
         Document doc = repository.createDocumentFromSource(templateId, path, properties);
