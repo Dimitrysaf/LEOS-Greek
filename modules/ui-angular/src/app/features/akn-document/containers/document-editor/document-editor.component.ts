ccf4cdb111a6de3c9d57b64b844d46f5a26ce275
Shailesh KHANDELWAL <Shailesh.KHANDELWAL@ext.ec.europa.eu>
2023-12-06 22:17:05 +0100

    Core #1295:Fixed coEdition notification issue

--- modules/ui-angular/src/app/features/akn-document/containers/document-editor/document-editor.component.ts
+++ modules/ui-angular/src/app/features/akn-document/containers/document-editor/document-editor.component.ts
@@ -421,7 +421,9 @@ export class DocumentEditorComponent
     this.coEditionWSService.latestMessage
       .pipe(takeUntil(this.destroy$))
       .subscribe((latestMessage) => {
-        if (latestMessage.info.presenterId !== presenterId)
+        if (latestMessage.info?.sessionId !== null &&
+          latestMessage.info.presenterId !== presenterId &&
+          latestMessage.info.documentId === this.documentRef) {
           this.appShellService.growl({
             severity: 'info',
             summary: 'Co Edition update',
@@ -432,7 +434,9 @@ export class DocumentEditorComponent
             )}`,
             life: 6000,
           });
+        }
       });
+
     this.loadingService.task$
       .pipe(takeUntil(this.destroy$))
       .subscribe((latestTask) => {
@@ -542,6 +546,7 @@ export class DocumentEditorComponent
   ngOnDestroy() {
     //remove every session related actions from the user and clean the document relaod if it is present
     this.coEditionWSService.setShouldReloadAfterUpdate();
+    this.coEditionWSService.removeDocumentCoEditInfo(this.documentRef);
     this.coEditionWSService.removeSession();
     this.reloadTrigger = 0;
     this.closeVersionView();
