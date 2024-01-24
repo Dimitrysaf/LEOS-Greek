3bc3d541f941774ea26499d79d273f9f79542945
adriacr <cristobal.adrian@ext.ec.europa.eu>
2024-01-24 17:09:40 +0100

    CORE#1361: Fixed save on EC save empty elements.

--- modules/js/src/main/js/editor/core/elementEditor.js
+++ modules/js/src/main/js/editor/core/elementEditor.js
@@ -442,27 +442,29 @@ define(function elementEditorModule(require) {
             var sibling = this.nextSibling;
             var isSiblingOL = (sibling && sibling.nodeType === Node.ELEMENT_NODE && sibling.tagName.toLowerCase() == 'ol');
             var isLastEditable = isLastEditableElement($(this).parent());
-            if (isLastEditable || (!$(this).siblings(":not(br)").length &&
-                    !$(this).parent().contents().filter(function() { return this.nodeType == Node.TEXT_NODE; }).length &&
-                    ($(this).parent().attr("refersto") || (isEmptyPrevSibling && isSiblingOL)))) {
+            if (isLastEditable || (isEmpty($(this).parent().get(0)) && ($(this).parent().attr("refersto") || (isEmptyPrevSibling && isSiblingOL)))) {
                 isEmptyList = true;
             }
         });
 
 
-        var isEmptyNewRefersToElement = false;
+        var isEmptyRefersToElement = false;
         $("#" + elementId).find("ol[data-akn-name='aknOrderedList']").each(function() {
-            var firstChild = this.firstChild, sibling = this.previousSibling;
-            var constainsRefersToElement = firstChild && firstChild.hasAttribute("refersto");
-            var newRefersToElement = sibling && sibling.nodeType === Node.ELEMENT_NODE && sibling.tagName.toLowerCase() == "p";
-            if (!constainsRefersToElement && newRefersToElement && !$(sibling).children(":not(br)").length &&
-                !$(sibling).contents().filter(function() { return this.nodeType == Node.TEXT_NODE; }).length) {
-                isEmptyNewRefersToElement = true;
+            var refersToElement = this.firstChild, newRefersToElement = this.previousSibling;
+            var containsRefersToElement = refersToElement && refersToElement.hasAttribute("refersto");
+            var containsNewRefersToElement = !containsRefersToElement && newRefersToElement && newRefersToElement.nodeType === Node.ELEMENT_NODE && newRefersToElement.tagName.toLowerCase() == "p";
+            if ((containsRefersToElement && isEmpty(refersToElement)) || (containsNewRefersToElement && isEmpty(newRefersToElement))) {
+                isEmptyRefersToElement = true;
             }
         });
 
 
-        return isEmptyElementFound || isEmptyList || isEmptyNewRefersToElement;
+        return isEmptyElementFound || isEmptyList || isEmptyRefersToElement;
+    }
+
+    function isEmpty(element) {
+        return element && !$(element).children(":not(br)").length &&
+            !$(element).contents().filter(function() { return this.nodeType == Node.TEXT_NODE && CKEDITOR.tools.trim(this.textContent).length > 0; }).length;
     }
 
     function isLastEditableElement(elementToTest){
