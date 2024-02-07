bd623ef3787410ea1ff8be53ec531ba3ec9da801
ANTON Marius <Marius.Anton@ext.ec.europa.eu>
2024-02-07 14:41:50 +0000

    Core #1435 - Validate availability of request scope

--- modules/services/src/main/java/eu/europa/ec/leos/services/document/ProposalServiceImpl.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/document/ProposalServiceImpl.java
@@ -44,7 +44,9 @@ import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.ScopeNotActiveException;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.context.request.RequestContextHolder;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
@@ -105,7 +107,14 @@ public abstract class ProposalServiceImpl implements ProposalService {
     public Proposal findProposal(String id) {
         LOG.trace("Finding Proposal... [id={}]", id);
         Proposal proposal = proposalRepository.findProposalById(id, true);
-        trackChangesContext.setTrackChangesEnabled(proposal.isTrackChangesEnabled());
+        try{
+            if (RequestContextHolder.getRequestAttributes() != null) {
+                trackChangesContext.setTrackChangesEnabled(proposal.isTrackChangesEnabled());
+            }
+        }catch(ScopeNotActiveException ex){
+            LOG.error("Request scoped bean accessed in async call");
+        }
+
         return proposal;
     }
 
