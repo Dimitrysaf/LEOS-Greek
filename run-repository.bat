50024330e7d925511170e8865982943dce5db484
adriacr <cristobal.adrian@ext.ec.europa.eu>
2024-02-07 14:58:15 +0100

    CORE#1411: Spring migration fixes for running in WL.

--- run-repository.bat
+++ run-repository.bat
@@ -19,7 +19,7 @@ echo "---------------------Repository-------------------------------------------
 cd ./tools/repository
 
 echo "---------------------Repository COMPILING...----------------------------------"
-call mvn clean install -Dmaven.test.skip=true
+call mvn clean install
 echo "---------------------Repository COMPILED.-------------------------------------"
 
 cd ./web
