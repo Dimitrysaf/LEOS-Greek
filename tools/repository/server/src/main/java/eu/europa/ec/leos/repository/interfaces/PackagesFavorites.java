38ec5de7b18b1967d2201195c16e1f5d9c79ebd9
camarfe <Felipe.CAMARGO-MACHADO@ext.ec.europa.eu>
2024-03-28 09:04:22 +0000

    Core #1578 Class changed back to interface

--- tools/repository/server/src/main/java/eu/europa/ec/leos/repository/interfaces/PackagesFavorites.java
+++ tools/repository/server/src/main/java/eu/europa/ec/leos/repository/interfaces/PackagesFavorites.java
@@ -5,39 +5,13 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.math.BigDecimal;
 
-public class PackagesFavorites {
+public interface PackagesFavorites {
 
-    private String creationDate = "";
-    private BigDecimal packageId = BigDecimal.ZERO;
-    private BigDecimal documentId = BigDecimal.ZERO;
-    private String ref = "";
-    private String title = "";
-    private BigDecimal favorite = BigDecimal.ZERO;
-
-    public PackagesFavorites() { }
-
-    public String getCreationDate() {
-        return creationDate;
-    }
-
-    public BigDecimal getPackageId() {
-        return packageId;
-    }
-
-    public BigDecimal getDocumentId() {
-        return documentId;
-    }
-
-    public String getRef() {
-        return ref;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public BigDecimal getFavorite() {
-        return favorite;
-    }
+    String getCreationDate();
+    BigDecimal getPackageId();
+    BigDecimal getDocumentId();
+    String getRef();
+    String getTitle();
+    BigDecimal getFavorite();
 
 }
