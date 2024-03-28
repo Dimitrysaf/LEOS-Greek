6978dd6a10fb705e789b8077bd6af2795babfeca
Kostas Kontos <kostas.kontos@vasscompany.com>
2024-03-28 13:07:00 +0200

    Core #1575: Fixed css of ribbon toolbar.

--- modules/ui-angular/src/app/features/akn-document/components/ribbon-toolbar-button/ribbon-toolbar-button.component.ts
+++ modules/ui-angular/src/app/features/akn-document/components/ribbon-toolbar-button/ribbon-toolbar-button.component.ts
@@ -4,6 +4,11 @@ import { Observable, takeUntil } from 'rxjs';
 import { IRibbonToolbarButton } from '../../models/document-actions.model';
 import { RibbonToolbarBaseComponent } from '../ribbon-toolbar-base/ribbon-toolbar-base.component';
 
+enum StyleMode {
+  Default,
+  NextChangeStyle,
+  PrevChangeStyle,
+}
 @Component({
   selector: 'app-ribbon-toolbar-button',
   templateUrl: './ribbon-toolbar-button.component.html',
@@ -14,12 +19,21 @@ export class RibbonToolbarButtonComponent
   implements OnInit
 {
   @Input() isOverflow = false;
-
+  styleMode = StyleMode.Default;
+  StyleMode = StyleMode;
   constructor() {
     super();
   }
 
   ngOnInit() {
     super.ngOnInit();
+    if (this.item.id === 'COMPARE_NEXT_CHANGE_ID') {
+      this.styleMode = StyleMode.NextChangeStyle;
+      console.log('Setting styleMode to NextChangeStyle');
+    } else if (this.item.id === 'COMPARE_PREV_CHANGE_ID') {
+      this.styleMode = StyleMode.PrevChangeStyle;
+      console.log('Setting styleMode to PrevChangeStyle');
+    }
+    console.log('Current styleMode:', this.styleMode);
   }
 }
