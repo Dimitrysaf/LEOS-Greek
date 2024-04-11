736335636aeeb68f3a4fbe69ee7786676262c124
camarfe <Felipe.CAMARGO-MACHADO@ext.ec.europa.eu>
2024-04-11 20:41:35 +0000

    Core #1669 Change in cypress module

--- modules/test-cypress/cypress/e2e/js/open-leos.cy.js
+++ modules/test-cypress/cypress/e2e/js/open-leos.cy.js
@@ -1,13 +1,13 @@
 describe('open leos', () => {
     it('leos login in dev', () => {
         cy.visit('https://intragate.development.ec.europa.eu/decide-drafting/ui/workspace');
-        cy.wait(2000);
+        cy.wait(5000);
         cy.get('#username').type('n00015oj');
         cy.get('.btn-primary').click();
-        cy.wait(2000);
+        cy.wait(5000);
         cy.get('#password').type('Sweden1234');
         cy.get('.btn-primary').click();
-        cy.wait(7000);
+        cy.wait(12000);
         cy.get('.eui-user-profile__infos-name').invoke('text').should('contain', 'Test1 Test');
     })
 })
