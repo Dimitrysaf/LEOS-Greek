8fd0a8a4d6df13cc9b204571388b2677a49573e4
camarfe <Felipe.CAMARGO-MACHADO@ext.ec.europa.eu>
2024-04-11 12:59:21 +0000

    Core #1669 Test cypress integration

--- modules/test-cypress/cypress.config.js
+++ modules/test-cypress/cypress.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
       on('file:preprocessor', cucumber());
     },
     experimentalStudio: true,
-    specPattern: "cypress/e2e/**/*.feature",
+    specPattern: ["cypress/e2e/**/*.feature", "cypress/e2e/recording/*.js"],
     watchForFileChanges: false
   },
 });
