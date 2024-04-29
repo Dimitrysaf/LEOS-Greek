de739f0e8084e8e07977198177d100681672972d
dasatya <satyabrata.das@ext.ec.europa.eu>
2024-04-29 14:33:08 +0000

    few fixes

--- modules/test-cypress/cypress/e2e/step_definitions/navigationPaneSteps.js
+++ modules/test-cypress/cypress/e2e/step_definitions/navigationPaneSteps.js
@@ -1,8 +1,9 @@
 import { Before, Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
 import navigationPane from "../pages/navigationPane";
 
-And('toc editing button is available', () => {
+And('toc editing button is displayed and enabled', () => {
     navigationPane.elements.editBtn().should('be.visible');
+    navigationPane.elements.editBtn().should('be.enabled');
 })
 
 
