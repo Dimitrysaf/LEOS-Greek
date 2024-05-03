e62c0fd69e195aa41a864090111098b193f532c5
dasatya <satyabrata.das@ext.ec.europa.eu>
2024-05-03 16:28:58 +0000

    test

--- modules/test-cypress/cypress.config.js
+++ modules/test-cypress/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   defaultCommandTimeout: 15000,
   viewportHeight: 1080,
   viewportWidth: 1920,
-  video: true,
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
