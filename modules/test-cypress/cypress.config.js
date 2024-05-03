580fdd304f8fb506aef1d09ee6105c584f857854
dasatya <satyabrata.das@ext.ec.europa.eu>
2024-05-03 15:32:39 +0000

    enable video generation for testcases

--- modules/test-cypress/cypress.config.js
+++ modules/test-cypress/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   defaultCommandTimeout: 15000,
   viewportHeight: 1080,
   viewportWidth: 1920,
-  video: false,
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
