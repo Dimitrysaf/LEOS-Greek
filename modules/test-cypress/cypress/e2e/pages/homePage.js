ab430438440683bcb0072c730ed4b3ec2e87ff5d
dasatya <satyabrata.das@ext.ec.europa.eu>
2024-06-04 10:38:46 +0000

    home page modification

---
+++ modules/test-cypress/cypress/e2e/pages/homePage.js
@@ -0,0 +1,25 @@
+import headerPage from './headerPage';
+
+class homePage extends headerPage {
+    elements = {
+        createProposalBtn: () => cy.contains('Create Proposal'),
+        uploadBtn: () => cy.contains('Upload'),
+        searchFilterInputBtn: () => cy.get("input[placeholder='Search for a proposal']"),
+        euiLabel: () => cy.get("eui-label.hero-text"),
+        viewAllActs: () => cy.contains("View all acts")
+        
+    }
+
+    clickCreateProposalBtn() {
+        this.elements.createProposalBtn().click();
+    }
+
+    clickUploadBtn() {
+        this.elements.uploadBtn().click();
+    }
+
+    clickViewAllActs(){
+        this.elements.viewAllActs().click();
+    }
+}
+export default new homePage();
---
+++ modules/test-cypress/cypress/e2e/step_definitions/homePageSteps.js
@@ -0,0 +1,11 @@
+import { Before, Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
+import homePage from "../pages/homePage";
+
+Then('user is on home page', () => {
+    homePage.elements.euiLabel().should('have.text', 'The online collaboration tool for drafting legislation');
+})
+
+When('click on view all acts button', () => {
+    homePage.clickViewAllActs();
+})
+
