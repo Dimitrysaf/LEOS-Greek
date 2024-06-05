547a003d94d6664c484778e966c47b1a9f305a9e
adriacr <cristobal.adrian@ext.ec.europa.eu>
2024-06-05 14:39:13 +0200

    CORE#1540: Package ref retrieved from proposal metadata.

--- modules/services/src/main/java/eu/europa/ec/leos/services/collection/CollectionContextMandateService.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/collection/CollectionContextMandateService.java
@@ -92,7 +92,7 @@ public class CollectionContextMandateService extends CollectionContextService {
         explanatoryContext.useTitle(messageHelper.getMessage("document.default.explanatory.title.default." + template));
         explanatoryContext.useActionMessageMap(actionMsgMap);
         explanatoryContext.useCollaborators(proposal.getCollaborators());
-        explanatoryContext.usePackageRef(metadata.getRef());
+        explanatoryContext.usePackageRef(proposal.getMetadata().get().getRef());
         Explanatory explanatory = explanatoryContext.executeCreateExplanatory();
         return explanatory;
     }
