a7627f373d239119a6334035356188ae7b8ddc88
Cristobal ADRIAN POLO <cristobal.adrian-polo@ext.ec.europa.eu>
2024-08-08 16:03:46 +0200

    Core #1958: Update to last spring version 5.3.37. Disabled websockets.

--- modules/services/src/main/java/eu/europa/ec/leos/services/config/CoEditionWebSocketConfig.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/config/CoEditionWebSocketConfig.java
@@ -7,9 +7,9 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Configuration
-@EnableAsync
-@EnableWebSocketMessageBroker
+//@Configuration
+//@EnableAsync
+//@EnableWebSocketMessageBroker
 public class CoEditionWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
