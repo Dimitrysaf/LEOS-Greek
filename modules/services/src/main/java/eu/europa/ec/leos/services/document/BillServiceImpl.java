f1983429120908dfe18485fac3a0f54229401608
nystepa <pascal.nysten@ec.ext.europa.eu>
2024-08-20 11:54:48 +0200

    1916 - Correction on access' right to contribution's documents and annotations

--- modules/services/src/main/java/eu/europa/ec/leos/services/document/BillServiceImpl.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/document/BillServiceImpl.java
@@ -18,7 +18,6 @@ import eu.europa.ec.leos.domain.common.TocMode;
 import eu.europa.ec.leos.domain.repository.Content;
 import eu.europa.ec.leos.domain.repository.common.VersionType;
 import eu.europa.ec.leos.domain.repository.document.Bill;
-import eu.europa.ec.leos.domain.repository.document.Proposal;
 import eu.europa.ec.leos.domain.repository.metadata.BillMetadata;
 import eu.europa.ec.leos.i18n.MessageHelper;
 import eu.europa.ec.leos.model.action.VersionVO;
