11d915a70678997742307b9a809c70c961c044bb
nystepa <pascal.nysten@ec.ext.europa.eu>
2024-10-29 12:08:25 +0100

    Core #2122 - Remove 'save' message while it's finished and solved issue of refresh while saving and closing ckeditor

--- modules/ui-angular/src/app/shared/services/loading.service.ts
+++ modules/ui-angular/src/app/shared/services/loading.service.ts
@@ -1,26 +1,73 @@
 import { Injectable } from '@angular/core';
-import { EuiAppShellService } from '@eui/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import {EuiAppShellService, EuiGrowlService} from '@eui/core';
+import {BehaviorSubject, debounceTime, Observable, takeUntil} from 'rxjs';
+import {TranslateService} from "@ngx-translate/core";
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoadingService {
-  loading$: Observable<boolean>;
-  task$: Observable<{ taskName: string; key: string; ongoing: boolean }>;
+  private tasksOngoing: { name: string; key: string }[] = [];
 
   private loadingBS = new BehaviorSubject<boolean>(false);
-  private taskBS = new BehaviorSubject<{
-    taskName: string;
-    key: string;
-    ongoing: boolean;
-  }>({ taskName: null, key: null, ongoing: false });
 
-  constructor(private euiAppShellService: EuiAppShellService) {
+  constructor(private euiAppShellService: EuiAppShellService, private appShellService: EuiGrowlService, private translate: TranslateService) {
     this.loadingBS.asObservable().subscribe((val) => {
       this.euiAppShellService.isBlockDocumentActive = val;
     });
-    this.task$ = this.taskBS.asObservable();
+  }
+
+  showOrHideTask(taskName: string, key: string, ongoing: boolean) {
+    if (ongoing) {
+      if (
+        !this.tasksOngoing.some(
+          (i) => i.name === taskName && i.key === key,
+        )
+      ) {
+        this.tasksOngoing.push({
+          name: taskName,
+          key: key,
+        });
+      }
+    } else if (
+      this.tasksOngoing.some(
+        (i) => i.name === taskName && i.key === key,
+      )
+    ) {
+      this.tasksOngoing = this.tasksOngoing.filter(
+        (item) =>
+          item.name !== taskName && item.key !== key,
+      );
+    }
+    let target = '';
+    this.tasksOngoing
+      .filter(
+        (value, index, array) =>
+          index === array.findIndex((item) => item.name === value.name),
+      )
+      .forEach(
+        (c) =>
+          (target =
+            target +
+            ' ' +
+            this.translate.instant('task.' + c.name + '.ongoing') +
+            ' '),
+      );
+    if (this.tasksOngoing.length > 0) {
+      this.appShellService.growl({
+        severity: 'info',
+        summary: 'Tasks ongoing',
+        detail: target,
+        sticky: true,
+      });
+    } else {
+      this.appShellService.growl({
+        severity: 'info',
+        summary: 'Tasks over',
+        detail: target,
+        life: 1,
+      });
+    }
   }
 
   setLoading(loading: boolean) {
@@ -28,10 +75,10 @@ export class LoadingService {
   }
 
   setTaskOngoing(taskName: string, key: string) {
-    this.taskBS.next({ taskName, key, ongoing: true });
+    this.showOrHideTask(taskName, key, true );
   }
 
   setTaskOver(taskName: string, key: string) {
-    this.taskBS.next({ taskName, key, ongoing: false });
+    this.showOrHideTask(taskName, key, false );
   }
 }
