456742260dbd98a826c41c93258b175353ce08b7
vellabh <bhanu-prasad.vellanki@ext.ec.europa.eu>
2024-10-31 01:51:37 +0100

    Core #2046 - Fix compilation issue in cypress tests.

--- modules/test-cypress/cypress/e2e/step_definitions/financialStatementSteps.js
+++ modules/test-cypress/cypress/e2e/step_definitions/financialStatementSteps.js
@@ -57,20 +57,3 @@ Then('repeated subparagraph should not exist', () => {
 Then('repeated subparagraph should have track changes action delete', () => {
     financialStatementPage.elements.repeatedSubparagraph().should('have.attr', 'leos:action').and('equal', 'delete');
 });
-
-function checkContentResult(element, datatable) {
-    element[0].childNodes.forEach((element, index) => {
-        if (element.nodeType === 1 && (datatable.raw().at(index).at(0).includes(",") || datatable.raw().at(index).at(0) !== "html")) {
-            const elementsArray = datatable.raw().at(index).at(0).split(",");
-            for (let elementIndex = 0; elementIndex < elementsArray.length; elementIndex++) {
-                expect(element.localName).equal(elementsArray[elementIndex]);
-                element = element.childNodes[0];
-            }
-            expect(element.textContent + "\"").equal(datatable.raw().at(index).at(1).substring(1));
-        }
-        if (element.nodeType === 1 && datatable.raw().at(index).at(0) === "html") {
-            const re = new RegExp(datatable.raw().at(index).at(1));
-            expect(true).equal(re.test(element.outerHTML));
-        }
-    })
-}
