7812c01bc3f4ada35a56fd5a8f8092b4353c75db
camarfe <Felipe.CAMARGO-MACHADO@ext.ec.europa.eu>
2024-11-05 14:25:46 +0000

    Core #2038 Undo of some changes and prepare to pipeline success

--- modules/ui-angular/src/app/features/proposal-view/containers/proposal-drafts/proposal-drafts.component.ts
+++ modules/ui-angular/src/app/features/proposal-view/containers/proposal-drafts/proposal-drafts.component.ts
@@ -234,7 +234,6 @@ export class ProposalDraftsComponent
   private populateView() {
     const getChildDocument = (type: DocumentType) =>
       this.proposal.childDocuments.find((d) => d.category === type) ?? null;
-    this.createOptions = JSON.parse(this.proposal.creationOptions);
     this.coverpage = getChildDocument('COVERPAGE');
     this.memorandum = getChildDocument('MEMORANDUM');
     this.document = getChildDocument('BILL');
