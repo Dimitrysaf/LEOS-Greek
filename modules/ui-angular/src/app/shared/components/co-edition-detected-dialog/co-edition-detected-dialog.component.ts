f7d4263119825c3b83a9ba7d13aa25c793f79e71
adriacr <cristobal.adrian@ext.ec.europa.eu>
2024-11-08 09:04:17 +0100

    Core#2107: Fixed CoEdition elements showed + format issues.

--- modules/ui-angular/src/app/shared/components/co-edition-detected-dialog/co-edition-detected-dialog.component.ts
+++ modules/ui-angular/src/app/shared/components/co-edition-detected-dialog/co-edition-detected-dialog.component.ts
@@ -1,15 +1,13 @@
 import {
-  ChangeDetectionStrategy,
   Component,
-  EventEmitter,
+  Inject,
   OnChanges,
   OnInit,
-  Output,
   SimpleChanges,
   ViewChild,
 } from '@angular/core';
-import { EuiDialogComponent } from '@eui/components/eui-dialog';
 
+import { DIALOG_COMPONENT_CONFIG, EuiDialogComponent } from '@eui/components/eui-dialog';
 import { CoEditionVO } from '@/shared/models/coEditionVO.model';
 import { CoEditionServiceWS } from '@/shared/services/coEdition.websocket.service';
 
@@ -18,25 +16,37 @@ import { CoEditionServiceWS } from '@/shared/services/coEdition.websocket.servic
   templateUrl: './co-edition-detected-dialog.component.html',
 })
 export class CoEditionDetectedDialogComponent implements OnInit, OnChanges {
+
   @ViewChild('coEditionDetectedDialog')
   coEditionDetectedDialog: EuiDialogComponent;
-  coEditionsVO: { [key: string]: CoEditionVO[] };
-  constructor(private coEditionService: CoEditionServiceWS) {
-    this.coEditionService.getDocCoEditionInfo().subscribe((c: any) => {
-      this.coEditionsVO = c;
-    });
+  coEditionsVO: CoEditionVO[];
+
+  constructor(@Inject(DIALOG_COMPONENT_CONFIG) private config, private coEditionService: CoEditionServiceWS) {
   }
 
   ngOnChanges(changes: SimpleChanges): void {}
+
+  ngOnInit() {
+    this.coEditionService.getDocCoEditionInfo()
+      .subscribe((coEdits: Record<string, CoEditionVO[]>) => {
+        for (const key in coEdits) {
+          if (((this.config.coEditionAction === 'EDIT_ELEMENT') && (key !== 'null') && (key === this.config.elementEditedId)) ||
+            ((this.config.coEditionAction === 'EDIT_TOC') && (key === 'null'))) {
+            this.coEditionsVO = coEdits[key];
+            break;
+          }
+        }
+      });
+  }
 
   handleContinue() {
     this.coEditionDetectedDialog.accept.emit();
     this.coEditionDetectedDialog.closeDialog();
   }
+
   handleCancel() {
     this.coEditionDetectedDialog.dismiss.emit();
     this.coEditionDetectedDialog.closeDialog();
   }
 
-  ngOnInit(): void {}
 }
