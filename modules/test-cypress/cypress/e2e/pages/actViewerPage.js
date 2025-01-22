43e8efe307e9dd29159b2ccd87c13a55f128824d
dasatya <satyabrata.das@ext.ec.europa.eu>
2025-01-22 14:59:10 +0000

    [Cypress-Automation] ReOrdering Of Annex Some testcase improvement

--- modules/test-cypress/cypress/e2e/pages/actViewerPage.js
+++ modules/test-cypress/cypress/e2e/pages/actViewerPage.js
@@ -12,23 +12,19 @@ class actViewerPage {
         coverPageLink: () => cy.get('.eui-tab-content-wrapper eui-card').contains('Cover Page'),
         expMemoLink: () => cy.get('.eui-tab-content-wrapper eui-card').contains('Explanatory Memorandum'),
         financialStatementLink: () => cy.get('.eui-tab-content-wrapper eui-card').contains('Legislative Financial and Digital Statement'),
-     //sapna   annexesSection: () => cy.get('.eui-tab-content-wrapper eui-card').contains('Annexes'),
         annexesSection: () => cy.get('div.eui-u-flex-grow > div > strong').contains('Annexes'),
-        // AddBtnfinancialStatement: () => cy.xpath("//div/*[text()='Financial Statement']//ancestor::div[contains(@class,'eui-u-flex')]//button//span[text()=' Add ']"),
+        // AddBtnFinancialStatement: () => cy.xpath("//div/*[text()='Financial Statement']//ancestor::div[contains(@class,'eui-u-flex')]//button//span[text()=' Add ']"),
         // financialStatementLink: () => cy.xpath("//a/*[text()='Financial Statement']"),
-        // deleteBtnfinancialStatement: () => cy.xpath("//a/*[text()='Financial Statement']//ancestor::div[contains(@class,'eui-u-flex')]//button//span[text()=' Delete ']"),
+        // deleteBtnFinancialStatement: () => cy.xpath("//a/*[text()='Financial Statement']//ancestor::div[contains(@class,'eui-u-flex')]//button//span[text()=' Delete ']"),
+        // proposalDetailsTab: () => this.elements.tabItem().contains('Details'),
         activeTab: () => cy.get('div.eui-tab-item--active'),
         tabItem: () => cy.get('div.eui-tabs__items-wrapper .eui-tab-item'),
         draftsTab: () => this.elements.tabItem().contains('Drafts'),
         detailsTab: () => this.elements.tabItem().contains('Details'),
         milestoneTab: () => this.elements.tabItem().contains('Milestones'),
         collaboratorsTab: () => this.elements.tabItem().contains('Collaborators'),
-        // proposalDetailsTab: () => this.elements.tabItem().contains('Details'),
         AddBtnAnnex: () => cy.xpath("//div/*[text()='Annexes']//ancestor::div[contains(@class,'eui-u-flex')]//button//span[text()=' Add ']"),
-        countOfRows:()=>cy.xpath("//table/tbody/tr"),
-        reorderButon:()=>cy.xpath("//div/*[text()='Annexes']//ancestor::div[contains(@class,'eui-u-flex')]//button/span[text()=' Reorder ']"),
-        //editDialogbox:()=>cy.xpath("//div/h5[@id='headerTitle']"),
-        editDialogueCloseButton:()=>cy.xpath("//button/span[text()=' Close ']"),
+        reOrderBtn:()=> cy.xpath("//div/*[text()='Annexes']//ancestor::div[contains(@class,'eui-u-flex')]//button/span[text()=' Reorder ']"),
         addFinancialStatementBtn: () => cy.xpath("//div/*[text()='Financial Statement']//ancestor::div[contains(@class,'eui-u-flex')]//button//span[text()=' Add ']"),
         deleteFinancialStatementBtn: () => cy.xpath("//*[text()='Financial Statement']//ancestor::div[contains(@class,'eui-u-flex')]//button//span[text()=' Delete ']"),
         noAnnexPresent: () => cy.contains("There is no annex to this document"),
@@ -42,54 +38,7 @@ class actViewerPage {
         confidentialityLevelLabelValue: () => this.elements.rowLabel().contains('Confidentiality Level').closest('div').next('div'),
         eeARelevanceCheckBoxValue: () => this.elements.rowLabel().contains('EEA Relevance').prev('input'),
         favouriteIcon: () => this.elements.appProposalHeader().find("eui-icon-svg svg[class*='eui-bookmark']"),
-        changeTitle:()=>cy.xpath("//div/button[contains(text(), 'Change title')]"),
-        editTitleAnnexDialogue:()=>cy.xpath("//div[@id='dialogContent']//h5"),
-        changeTitleName :()=>cy.xpath("//div[@id='bodyTemplatePortalId']//input"),
-        saveFromEditTitleDialogue:()=>cy.xpath("//button/span[text()=' Save ' ]"),
-        clearAnnexPreviousTitle:()=>cy.xpath( "//div[@id='bodyTemplatePortalId']//input"),
-        deleteAnnexFromActView:()=>cy.xpath("//eui-dropdown-content//button[text()='Delete']"),
-        deleteAnnexButton:()=>cy.xpath("//button/span[text()=' Delete ']"),
-        deleteFromDeleteDialogueBox:()=>cy.xpath("//div/eui-dialog-header/h3"),
-        editAnnexOrderDialogue:()=>cy.xpath("//h5[@id='headerTitle']"),
-        closeButtonFromEditAnnexOrder :()=>cy.xpath("//div/button/span[text()=' Close ']"),
-
         }
-    clickDeleteFromActView(){
-        this.elements.deleteAnnexFromActView().click();
-    }
-
-    closeButtonFromEditAnnexOrder(){
-        this.elements.closeButtonFromEditAnnexOrder().click();
-    }
-
-    dragAndDrop(){
-        cy.xpath("//div[contains(@id, 'cdk-drop-list-')]//div[text()=' Annex ']")
-            .should('exist')
-            .trigger('dragstart'); // Start the drag operation
-
-        cy.xpath("//div[contains(@id, 'cdk-drop-list-')]//div[text()=' Annex1 ']")
-            .should('exist')
-            .trigger('dragenter') // Enter the drop target
-            .trigger('dragover')  // Hover over the drop target
-            .trigger('drop');     // Perform the drop
-
-    }
-
-   AnnexDeletion(){
-    this.elements.deleteAnnexButton().click()
-   }
-
-    clearAnnexPreviousTitle(){
-        this.elements.clearAnnexPreviousTitle().clear()
-    }
-
-    clickSaveFromEditTitleDialogue(){
-        this.elements.saveFromEditTitleDialogue().click();
-    }
-
-    clickChangeTitle(){
-        this.elements.changeTitle().click();
-    }
 
     clickCloseBtn(){
         this.elements.closeBtn().click();
@@ -121,19 +70,7 @@ class actViewerPage {
     }
 
     clickReorderButton(){
-        this.elements.reorderButon().click();
-    }
-
-    countOfAnnexRows (){
-        this.elements.countOfRows()
-    }
-
-    editDialogueBOX(){
-        this.elements.editDialogbox();
-    }
-
-    editDialogueCloseButton(){
-        this.elements.editDialogueCloseButton().click()
+        this.elements.reOrderBtn().click();
     }
 
     clickNthAnnex(annexNumber){
@@ -198,11 +135,6 @@ class actViewerPage {
 
     clickFavouriteIcon() {
         this.elements.favouriteIcon().click({force:true});
-    }
-
-    lastElementOfTheAnnexRow(){
-        cy.xpath("//tbody/tr/td[4]").last().click()
-
     }
 }
 export default new actViewerPage();
