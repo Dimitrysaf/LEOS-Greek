561a060de01ae7218f086bc3602a4a636f08a2b7
adriacr <cristobal.adrian@ext.ec.europa.eu>
2025-02-28 15:06:13 +0100

    CORE#2386: Added output descriptor to convert service. Removed custom multipart and added to pilot.

--- tools/akn4euutil/server/src/main/java/eu/europa/ec/digit/leos/pilot/export/service/impl/LeosConvertDocumentServiceImpl.java
+++ tools/akn4euutil/server/src/main/java/eu/europa/ec/digit/leos/pilot/export/service/impl/LeosConvertDocumentServiceImpl.java
@@ -20,6 +20,11 @@ import org.springframework.stereotype.Service;
 public class LeosConvertDocumentServiceImpl implements ConvertDocumentService {
     @Override
     public byte[] convertDocument(byte[] input, String inputFilename) {
+        return this.convertDocument(input, inputFilename, null);
+    }
+
+    @Override
+    public byte[] convertDocument(byte[] input, String inputFilename, String outputDescriptor) {
         return input;
     }
 }
