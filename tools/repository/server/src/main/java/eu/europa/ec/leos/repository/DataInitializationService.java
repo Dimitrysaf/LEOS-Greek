5304bad29a9756a5aee95838bbba8de5f22186b1
Jon GONZALEZ SANTAMARIA <jon.gonzalez-santamaria@ext.ec.europa.eu>
2025-04-04 16:48:35 +0200

    Core #2335: Created new folders for template configuration

--- tools/repository/server/src/main/java/eu/europa/ec/leos/repository/DataInitializationService.java
+++ tools/repository/server/src/main/java/eu/europa/ec/leos/repository/DataInitializationService.java
@@ -1,7 +1,5 @@
 package eu.europa.ec.leos.repository;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
@@ -17,25 +15,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Order
 @Profile("h2")
-public class DataInitializationService {
-    private static final Logger LOG = LoggerFactory.getLogger(DataInitializationService.class.getName());
+public class DataInitializationService extends DataUploadService {
 
-    private final JdbcTemplate jdbcTemplate;
     private final ResourceLoader resourceLoader;
-    private final ResourcePatternResolver resourcePatternResolver;
 
-    @Value("${repository.template.path}")
-    private String configFolderPath;
     @Value("${repository.workspace.path}")
     private String documentFolderPath;
     @Value("${repository.data.script.path}")
@@ -57,6 +46,7 @@ public class DataInitializationService {
         for (String instance : instances) {
             loadConfigDataFromFilesInFolder(instance);
         }
+        loadConfigDataFromFilesInCustomFolder();
 
         loadDocumentDataFromFilesInFolder();
     }
@@ -84,86 +74,6 @@ public class DataInitializationService {
         }
     }
 
-    public void loadConfigDataFromFilesInFolder(String subdirectory) {
-        try {
-            LOG.info("Loading resources from: classpath:{}/{}/*", configFolderPath, subdirectory);
-            Resource[] resources = resourcePatternResolver.getResources("classpath*:" + configFolderPath + "/*/" + subdirectory + "/*");
-
-            // Extracting unique version folders
-            List<String> versionFolders = Arrays.stream(resources)
-                    .map(resource -> {
-                        try {
-                            String[] segments = resource.getURL().getPath().split("/");
-                            return segments[segments.length - 3]; // Extract version folder name
-                        } catch (IOException e) {
-                            LOG.error("Error extracting version folder from resource: {}", resource, e);
-                            return null;
-                        }
-                    })
-                    .filter(versionFolder -> versionFolder != null)
-                    .distinct()
-                    .sorted()
-                    .collect(Collectors.toList());
-
-            if (versionFolders.isEmpty()) {
-                LOG.warn("No version folders found in path: classpath:{}/{}/*", configFolderPath, subdirectory);
-                return;
-            }
-
-            for (String versionFolder : versionFolders) {
-                LOG.info("Processing version folder: {}", versionFolder);
-                Resource[] fileResources = resourcePatternResolver.getResources("classpath*:" + configFolderPath + "/" + versionFolder + "/" + subdirectory + "/*");
-                for (Resource fileResource : fileResources) {
-                    if (fileResource.exists() && fileResource.isReadable()) {
-                        String fileContent = getFileContent(fileResource);
-                        String fileName = fileResource.getFilename();
-                        String categoryCode = "CONFIG";  //TODO this we need to find a way to dynamically detect from directory structure the config type
-
-                        //check if this config exists
-                        String checkConfigQuery = "SELECT COUNT(*) FROM config WHERE name = ?";
-                        int countConfig = jdbcTemplate.queryForObject(checkConfigQuery, new Object[]{fileName.substring(0, fileName.lastIndexOf("."))}, Integer.class);
-
-                        if (countConfig == 0) {
-                            String insertQuery = "INSERT INTO CONFIG" +
-                                    "(NAME, CATEGORY_ID, AUDIT_C_BY, AUDIT_C_DATE)" +
-                                    "VALUES(?, (select id from CONFIG_CATEGORIES where CATEGORY_CODE = ?), 'admin', current_timestamp)";
-
-                            jdbcTemplate.update(insertQuery, fileName.substring(0, fileName.lastIndexOf(".")), categoryCode);
-                        }
-
-                        // check if this version exists for this config
-                        String selectQuery = "SELECT COUNT(*) FROM config_version WHERE version_label = ? " +
-                                "AND config_id = (SELECT id FROM config WHERE name = ?)";
-
-                        int count = jdbcTemplate.queryForObject(selectQuery,  new Object[]{versionFolder, fileName.substring(0, fileName.lastIndexOf("."))}, Integer.class);
-
-
-                        if (count == 0) {
-                            String insertQuery = "INSERT INTO CONFIG_VERSION (CONFIG_ID, VERSION_LABEL, VERSION_SERIES_ID, VERSION_TYPE, IS_LATEST_MAJOR_VERSION, " +
-                                    "IS_LATEST_VERSION, IS_MAJOR_VERSION, IS_VERSION_SERIES_CHECKED_OUT, AUDIT_C_BY, AUDIT_C_DATE, " +
-                                    "AUDIT_LAST_M_DATE, AUDIT_LAST_M_BY, IS_IMMUTABLE) " +
-                                    "VALUES ((SELECT id FROM CONFIG WHERE NAME=?), ?, '1', null, 1, 1, 1, 0, 'admin/admin', " +
-                                    "current_timestamp, current_timestamp, 'admin/admin', 0)";
-
-                            //set is_latest_version flag for other versions to false
-                            String updateQuery = "UPDATE CONFIG_VERSION SET IS_LATEST_VERSION = 0 WHERE CONFIG_ID IN (SELECT ID FROM CONFIG WHERE NAME = ?) AND version_label != ?";
-
-                            jdbcTemplate.update(insertQuery, fileName.substring(0, fileName.lastIndexOf(".")), versionFolder);
-                            jdbcTemplate.update(updateQuery, fileName.substring(0, fileName.lastIndexOf(".")), versionFolder);
-                        }
-
-                        String sql = "INSERT INTO CONFIG_CONTENT (VERSION_ID, CONTENT, CONTENT_STREAM_MIME_TYPE, CONTENT_STREAM_FILENAME, CONTENT_STREAM_ID, CONTENT_STREAM_LENGTH, AUDIT_C_BY, AUDIT_C_DATE, AUDIT_LAST_M_DATE, AUDIT_LAST_M_BY) " +
-                                "VALUES ((SELECT id FROM CONFIG_VERSION WHERE CONFIG_ID IN (SELECT ID FROM CONFIG WHERE NAME = ?) and VERSION_LABEL = ?), ?, 'application/xml', ?, '0', '0', 'admin', " +
-                                "current_timestamp, current_timestamp, 'admin');";
-                        jdbcTemplate.update(sql, fileName.substring(0, fileName.lastIndexOf(".")), versionFolder, fileContent, fileName);
-                    }
-                }
-            }
-        } catch (IOException e) {
-            LOG.error("Unable to read files from the folder", e);
-        }
-    }
-    
     public void loadDocumentDataFromFilesInFolder() {
         try {
             // Get the physical path of the config folder on the classpath
@@ -198,20 +108,5 @@ public class DataInitializationService {
         } catch (IOException e) {
             LOG.error("Unable to read files from the folder", e);
         }
-    }
-
-    private String getFileContent(Resource fileResource) {
-        try (InputStream inputStream = fileResource.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            StringBuilder fileContent = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                fileContent.append(line).append("\n");
-            }
-            return fileContent.toString();
-        } catch (IOException e) {
-            LOG.error("Unable to read files from the folder", e);
-        }
-        return null;
     }
 }
