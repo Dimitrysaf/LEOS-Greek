7915524f513cf105b5f5df05cbc0291b14ce3c5a
Nikhil GUPTA <nikhil.gupta@ext.ec.europa.eu>
2025-06-12 15:14:13 +0200

    Merge branch '2602-autonomous-acts-drop-down-for-signature-options-combo' into 'EPIC_autonomous_acts'
    Core #2602 combo for alternatives
    
    See merge request leos/core!2655

---
+++ modules/js/src/main/js/editor/profiles/inlineAknSignatoryAutonomousAct.js
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2024 European Union
+ *
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ *     https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
+; // jshint ignore:line
+define(function aknSignatoryProfileModule(require) {
+    "use strict";
+
+    // require profile dependencies, if needed
+    // e.g. ckEditor, plugins or utilities
+    var transformationConfigManager = require("transformer/transformationConfigManager");
+    var pluginTools = require("plugins/pluginTools");
+    var $ = require('jquery');
+
+    var plugins = [];
+    plugins.push(require("plugins/leosInlineSave/leosInlineSavePlugin"));
+    plugins.push(require("plugins/leosInlineCancel/leosInlineCancelPlugin"));
+    plugins.push(require("plugins/leosInlineEditor/leosInlineEditorPlugin"));
+    plugins.push(require("plugins/leosTransformer/leosTransformerPlugin"));
+    plugins.push(require("plugins/leosFixNestedPs/leosFixNestedPsPlugin"));
+    plugins.push(require("plugins/aknHtmlBold/aknHtmlBoldPlugin"));
+    plugins.push(require("plugins/aknHtmlItalic/aknHtmlItalicPlugin"));
+    plugins.push(require("plugins/leosShowblocks/leosShowblocksPlugin"));
+    plugins.push(require("plugins/aknAuthorialNote/aknAuthorialNotePlugin"));
+    plugins.push(require("plugins/aknBlockSignatory/aknBlockSignatoryPlugin"));
+    plugins.push(require("plugins/aknSignature/aknSignaturePlugin"));
+    plugins.push(require("plugins/aknOrganization/aknOrganizationPlugin"));
+    plugins.push(require("plugins/aknRole/aknRolePlugin"));
+    plugins.push(require("plugins/aknPerson/aknPersonPlugin"));
+    plugins.push(require("plugins/aknHtmlSuperScript/aknHtmlSuperScriptPlugin"));
+    plugins.push(require("plugins/aknHtmlSubScript/aknHtmlSubScriptPlugin"));
+    plugins.push(require("plugins/leosAttrHandler/leosAttrHandlerPlugin"));
+    plugins.push(require("plugins/leosPaste/leosPastePlugin"));
+    plugins.push(require("plugins/leosCrossReference/leosCrossReferencePlugin"));
+    plugins.push(require("plugins/leosFloatingSpace/leosFloatingSpacePlugin"));
+    plugins.push(require("plugins/leosWidget/leosWidgetPlugin"));
+    plugins.push(require("plugins/leosMessageBus/leosMessageBusPlugin"));
+    plugins.push(require("plugins/leosDropHandler/leosDropHandlerPlugin"));
+    plugins.push(require("plugins/leosXmlEntities/leosXmlEntitiesPlugin"));
+    plugins.push(require("plugins/leosTextCaseChanger/leosTextCaseChangerPlugin"));
+    plugins.push(require("plugins/leosSpecialChar/leosSpecialCharPlugin"));
+    plugins.push(require("plugins/leosPreventElementDeletion/leosPreventElementDeletionPlugin"));
+    plugins.push(require("plugins/leosSpellChecker/leosSpellCheckerPlugin"));
+    plugins.push(require("plugins/leosPreventSelectAll/leosPreventSelectAllPlugin"));
+    plugins.push(require("plugins/leosTrackChanges/config/leosTrackChangesInsPlugin"));
+    plugins.push(require("plugins/leosTrackChanges/config/leosTrackChangesDelPlugin"));
+    plugins.push(require("plugins/leosTrackChanges/leosTrackChangesPlugin"));
+    plugins.push(require("plugins/leosTextHighlight/leosTextHighlightPlugin"));
+    plugins.push(require("plugins/leosDapInfo/leosDapInfo"));
+    plugins.push(require("plugins/leosAlternativesSignatory/leosAlternativesSignatoryPlugin"));
+    plugins.push(require("plugins/leosToolbar/leosToolbarPlugin"));
+
+    var pluginNames=[];
+    var specificConfig={};
+    $.each(plugins, function( index, value ) {
+        pluginNames.push(value.name);
+        specificConfig= $.extend( specificConfig,  value.specificConfig);
+    });
+    var transformationConfigResolver = transformationConfigManager.getTransformationConfigResolverForPlugins(pluginNames);
+
+    // holds ckEditor external plugins names
+    var externalPluginsNames = [];
+    pluginTools.addExternalPlugins(externalPluginsNames);
+    var extraPlugins = pluginNames.concat(externalPluginsNames).join(",");
+    var leosPasteFilter = pluginTools.createFilterList(transformationConfigResolver);
+
+    var profileName = "AKN Signatory";
+
+    // create profile configuration
+    var profileConfig = {
+        // user interface language localisation
+        language: "en",
+
+        // custom configuration to load (none if empty)
+        customConfig: "",
+        // comma-separated list of plugins to be loaded
+        plugins: "toolbar,wysiwygarea,elementspath," +
+            "clipboard,undo,pastefromword,basicstyles,enterkey," +
+            "specialchar,button,dialog,dialogui,contextmenu,menubutton,widget,colorbutton",
+        // comma-separated list of toolbar button names that must not be rendered
+        removeButtons: "Italic,Bold,Underline,Strike,TextColor,PasteFromWord",
+        // comma-separated list of additional plugins to be loaded
+        extraPlugins: extraPlugins,
+        // disable Advanced Content Filter (allow all content)
+        allowedContent: true,
+        //only allow elements configured in transformer
+        pasteFilter:leosPasteFilter,
+        defaultPasteElement:'text',
+        // height of the editing area
+        height : 522,
+        //show toolbar on startup
+        startupFocus: 'end',
+        //Use native spellchecker
+        disableNativeSpellChecker: false,
+        // LEOS-2887 removing tooltip title
+        title: false,
+        // toolbar groups arrangement, optimised for a single toolbar row
+        toolbarGroups : [ {
+            name : "save"
+        }, {
+            name : "document",
+            groups : [ "document", "doctools" ]
+        }, {
+            name : "clipboard",
+            groups : [ "clipboard", "undo" ]
+        }, {
+            name : "editing",
+            groups : [ "selection", "spellchecker" ]
+        }, {
+            name : "forms"
+        }, {
+            name : "basicstyles",
+            groups : [ "basicstyles", "cleanup" ]
+        }, {
+            name : "paragraph"
+        }, {
+            name : "insert"
+        }, {
+            name : "styles"
+        }, {
+            name : "tools"
+        }, {
+            name : "alternatives"
+        }, {
+            name : "others"
+        },{
+            name : "mode"
+        }, {
+            name : "about"
+        },{
+            name: 'trackChanges', groups: ['toggleDisplay']
+        },{
+            name : "colors"
+        } ,{
+            name: 'dapInfo'
+        } ],
+        isAlternative: true
+    };
+    // adding the specific configs coming from the plugins.
+    profileConfig = $.extend( profileConfig,  specificConfig);
+    // create profile definition
+    var profileDefinition = {
+        name: profileName,
+        config: profileConfig,
+        transformationConfigResolver:transformationConfigResolver
+    };
+
+    return profileDefinition;
+});
