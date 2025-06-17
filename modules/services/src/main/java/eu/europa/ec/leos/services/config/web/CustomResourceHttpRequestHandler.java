bdde1a7888d9abf245846ee53f8c7cf172be70dc
candref <Andre-Filipe.COSTA-RESTOLHO@ext.ec.europa.eu>
2025-06-17 17:06:09 +0000

    Core #2676 - Moved classes from web to services

--- modules/services/src/main/java/eu/europa/ec/leos/services/config/web/CustomResourceHttpRequestHandler.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/config/web/CustomResourceHttpRequestHandler.java
@@ -11,7 +11,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Licence for the specific language governing permissions and limitations under the Licence.
  */
-package eu.europa.ec.leos.services.config;
+package eu.europa.ec.leos.services.config.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
