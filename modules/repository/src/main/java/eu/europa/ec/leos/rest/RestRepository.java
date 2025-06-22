8d658a7d22bd393627a0bed0bafcb42698fd7035
candref <Andre-Filipe.COSTA-RESTOLHO@ext.ec.europa.eu>
2025-06-22 15:09:31 +0000

    Core #2683 - Migration to Spring Boot 2.7

--- modules/repository/src/main/java/eu/europa/ec/leos/rest/RestRepository.java
+++ modules/repository/src/main/java/eu/europa/ec/leos/rest/RestRepository.java
@@ -171,6 +171,7 @@ public class RestRepository extends AbstractRestClient {
     private String leosRestpackageDeleteCollaborators;
 
     @Autowired
+    @Qualifier("restProperties")
     private RepositoryPropertiesMapper repositoryPropertiesMapper;
 
     private String getUrl(String resourceUrl) {
