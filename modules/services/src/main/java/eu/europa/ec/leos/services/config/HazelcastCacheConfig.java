840620e0bf941e8f2fbb55388c3fab281a0de6ae
Andre Restolho <andre-filipe.costa-restolho@ext.ec.europa.eu>
2025-06-23 16:36:03 +0100

    Core #2682 - Fixed issue with Hazelcast config

--- modules/services/src/main/java/eu/europa/ec/leos/services/config/HazelcastCacheConfig.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/config/HazelcastCacheConfig.java
@@ -80,8 +80,6 @@ public class HazelcastCacheConfig {
         // Create and set eviction config (includes size configuration in Hazelcast 5.x)
         EvictionConfig evictionConfig = new EvictionConfig();
         evictionConfig.setEvictionPolicy(EvictionPolicy.LFU);
-        evictionConfig.setMaxSizePolicy(MaxSizePolicy.PER_NODE);
-        evictionConfig.setSize(maxSize);
         mapConfig.setEvictionConfig(evictionConfig);
 
         if (name.equals("coEditionCache")) {
