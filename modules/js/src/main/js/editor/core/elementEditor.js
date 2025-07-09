11d7443c7fbadea3480378b5749d9555604eef1a
Felipe CAMARGO MACHADO <felipe.camargo-machado@ext.ec.europa.eu>
2025-07-09 15:06:21 +0200

    Merge branch '2662-Autonomous-Act-recital' into 'EPIC_autonomous_acts'
    Core #2662 add aknUnorderedList to validate Recitals in autonomous acts
    
    See merge request leos/core!2729

--- modules/js/src/main/js/editor/core/elementEditor.js
+++ modules/js/src/main/js/editor/core/elementEditor.js
@@ -618,7 +618,7 @@ define(function elementEditorModule(require) {
     }
     function hasNoValidLiContent(el) {
         var parent = $(el).parent()[0];
-        if($(el).is("li") && el.hasAttribute('data-akn-num') && (($(parent).attr("data-akn-name") == "NumberedBlockList") || ($(parent).attr("data-akn-name") == "UnNumberedBlockList"))){
+        if($(el).is("li") && el.hasAttribute('data-akn-num') && (($(parent).attr("data-akn-name") == "NumberedBlockList") || ($(parent).attr("data-akn-name") == "UnNumberedBlockList") || ($(parent).attr("data-akn-name") == "aknUnorderedList"))){
             for (const node of el.childNodes) {
                 if (node && node.nodeType === Node.TEXT_NODE) {
                     if (node.textContent.trim() !== '') {
