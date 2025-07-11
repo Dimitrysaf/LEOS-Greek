51c53a9199a3ee9e36e74c23a9b776c71ca1f44e
gnzaljo <jon.gonzalez-santamaria@ext.ec.europa.eu>
2025-07-11 10:42:49 +0000

    Core #2733: prevented hard deletion of content added by other users in articles and higher elements added with TC

--- modules/js/src/main/js/editor/plugins/leosTrackChanges/leosTrackChangesPlugin.js
+++ modules/js/src/main/js/editor/plugins/leosTrackChanges/leosTrackChangesPlugin.js
@@ -498,8 +498,7 @@ define(function leosTrackChangesPluginModule(require) {
 
                 // Used for CTRL-X, to get the content BEFORE been deleted
                 editable.attachListener(editor.document, "keydown", function(e) {
-                    if (!CKEDITOR.dialog?.getCurrent() && isTrackChangesEnabled && !core.isInsideTrackedHigherElement(editor, core.getUserId(editor)) &&
-                        (editor.getSelection().getRanges().length > 0)) {
+                    if (!CKEDITOR.dialog?.getCurrent() && isTrackChangesEnabled && (editor.getSelection().getRanges().length > 0)) {
                         var event = new EventWrapper(e);
                         if (e.data.$.ctrlKey && event.getKeyCode() === UTILS.KEYS.KEY_X) {
                             editor.execCommand("copy");
@@ -527,9 +526,7 @@ define(function leosTrackChangesPluginModule(require) {
 
                 // Delete functionality - key - catch snapshots
                 editable.attachListener(editor, "key", function(e) {
-                    if (!CKEDITOR.dialog?.getCurrent() && isTrackChangesEnabled && !core.isInsideTrackedHigherElement(editor, core.getUserId(editor)) &&
-                        (editor.getSelection().getRanges().length > 0)) {
-
+                    if (!CKEDITOR.dialog?.getCurrent() && isTrackChangesEnabled && (editor.getSelection().getRanges().length > 0)) {
                         var event = new EventWrapper(e);
 
                         // On delete functionality(prevents/backup of text)
