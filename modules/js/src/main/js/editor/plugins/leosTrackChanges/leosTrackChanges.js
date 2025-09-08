963040e23fc8e3a168d50f0234f6d9774b4e38c0
camarfe <Felipe.CAMARGO-MACHADO@ext.ec.europa.eu>
2025-09-08 08:06:20 +0000

    Fix to not correct rebase part of code

--- modules/js/src/main/js/editor/plugins/leosTrackChanges/leosTrackChanges.js
+++ modules/js/src/main/js/editor/plugins/leosTrackChanges/leosTrackChanges.js
@@ -775,7 +775,6 @@ define(function leosTrackChangesModule(require) {
             }
             for (var j = editableElements.count() - 1; j >= 0; j--) {
                 var edElementToProcess = editableElements.getItem(j);
-                var processedElements = [];
                 isElementDeleted = this.processListElement(editor, edElementToProcess, processedElements, actionName, isStructureTooComplex);
                 if (isElementDeleted || isStructureTooComplex[0]) {
                     break;
