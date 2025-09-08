c011601b964022ec6a1981327462a768b7bc478f
nystepa <pascal.nysten@ec.ext.europa.eu>
2025-09-08 12:33:19 +0200

    Core #2665 - Avoid removal of 'refersTo' attribute on person element in signature

--- modules/js/src/main/js/editor/transformer/attributeTransformer.js
+++ modules/js/src/main/js/editor/transformer/attributeTransformer.js
@@ -73,12 +73,11 @@ define(function attributeTransformerModule(require) {
             },
             perform: function() {
                 if (!!this.attrConfig.from
-                    && (this.fromElement.attributes[this.attrConfig.from.toLowerCase()] || this.fromElement.attributes[this.attrConfig.from.toLowerCase()] == "")) {
+                    && (this.fromElement.attributes[this.attrConfig.from.toLowerCase()]
+                        || (this.fromElement.attributes[this.attrConfig.from.toLowerCase()] == "" && !this.attrConfig.toValue))) {
                     this.toElement.attributes[this.attrConfig.to] = this.fromElement.attributes[this.attrConfig.from.toLowerCase()];
-                } else {
-                    if (this.attrConfig.toValue) {
-                        this.toElement.attributes[this.attrConfig.to] = this.attrConfig.toValue;
-                    }
+                } else if (this.attrConfig.toValue) {
+                    this.toElement.attributes[this.attrConfig.to] = this.attrConfig.toValue;
                 }
             }
         },
