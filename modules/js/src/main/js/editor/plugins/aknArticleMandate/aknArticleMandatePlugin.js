6b18474ad3dc183cce987cf202956e6d645cfdd9
ANTON Marius <Marius.Anton@ext.ec.europa.eu>
2025-09-09 11:38:10 +0000

    Core #2884 - add attributes for restore or remove

--- modules/js/src/main/js/editor/plugins/aknArticleMandate/aknArticleMandatePlugin.js
+++ modules/js/src/main/js/editor/plugins/aknArticleMandate/aknArticleMandatePlugin.js
@@ -142,6 +142,15 @@ define(function aknArticleMandatePluginModule(require) {
             akn : "leos:softdate",
             html : "data-akn-attr-softdate"
         }, {
+            akn: "leos:id-to-be-restored",
+            html: "data-akn-id-to-be-restored"
+        }, {
+            akn: "leos:renumber-origin",
+            html: "data-akn-renumber-origin"
+        }, {
+            akn: "leos:id-to-be-removed",
+            html: "data-akn-id-to-be-removed"
+        }, {
             akn : "leos:softmove_to",
             html : "data-akn-attr-softmove_to"
         }, {
@@ -194,6 +203,15 @@ define(function aknArticleMandatePluginModule(require) {
             }, {
                 akn : "leos:softdate",
                 html : "data-akn-attr-softdate"
+            }, {
+                akn: "leos:id-to-be-restored",
+                html: "data-akn-id-to-be-restored"
+            }, {
+                akn: "leos:renumber-origin",
+                html: "data-akn-renumber-origin"
+            }, {
+                akn: "leos:id-to-be-removed",
+                html: "data-akn-id-to-be-removed"
             }, {
                 akn : "xml:id",
                 html : "data-akn-heading-id"
