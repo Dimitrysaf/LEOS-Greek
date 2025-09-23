1cd09ebb8727f279c1342325f038f181c947ef9d
nystepa <pascal.nysten@ec.ext.europa.eu>
2025-09-23 15:06:14 +0200

    Core #2883 - Reset button for adoption info

--- modules/services/src/main/java/eu/europa/ec/leos/services/api/ApiServiceImpl.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/api/ApiServiceImpl.java
@@ -703,7 +703,6 @@ public abstract class ApiServiceImpl implements ApiService {
         LOG.trace(proposalRef);
         ProposalDetailsVO proposalDetails = new ProposalDetailsVO();
         ProposalDetailsLists proposalDetailsLists = proposalDetailsService.getProposalDetailsLists();
-        proposalDetails.setProposalDetailsLists(proposalDetailsLists);
         Set<MilestonesVO> milestonesVOs = new TreeSet<>(Comparator.comparing(MilestonesVO::getUpdatedDateAsDate).reversed());
         Proposal proposal = null;
         byte[] proposalXmlContent = new byte[0];
@@ -726,6 +725,7 @@ public abstract class ApiServiceImpl implements ApiService {
                     leosPackage = packageService.findPackageByPackageId(linkedPackage.getPackageId());
                 }
                 List<XmlDocument> documents = packageService.findDocumentsByPackagePath(leosPackage.getPath(), XmlDocument.class, false);
+                proposalDetailsLists = proposalDetailsService.populateTemplateSignatures(proposalDetailsLists, documents);
                 List<LegDocument> legDocuments = packageService.findDocumentsByPackageId(leosPackage.getId(), LegDocument.class, false, true);
                 FavouritePackageResponse favouritePackageResponse = packageService.getFavouritePackage(proposalRef, userId);
                 legDocuments.sort(Comparator.comparing(LegDocument::getLastModificationInstant).reversed());
@@ -763,6 +763,7 @@ public abstract class ApiServiceImpl implements ApiService {
                     milestonesVOsLock.unlockWrite(stamp);
                 }
                 proposalVO.setDetailsTabExclusions(getDetailsTabExclusions(proposal));
+                proposalDetails.setProposalDetailsLists(proposalDetailsLists);
                 proposalDetails.setDocument(proposalVO);
                 return Optional.of(proposalDetails);
             } catch (Exception e) {
