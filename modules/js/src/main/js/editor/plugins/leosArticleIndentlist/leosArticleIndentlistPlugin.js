a7640ff2c44c4221c8464b9070e95b207875b97e
camarfe <Felipe.CAMARGO-MACHADO@ext.ec.europa.eu>
2025-09-25 07:39:51 +0000

    Core #2909 Fix to cypress test failing

--- modules/js/src/main/js/editor/plugins/leosArticleIndentlist/leosArticleIndentlistPlugin.js
+++ modules/js/src/main/js/editor/plugins/leosArticleIndentlist/leosArticleIndentlistPlugin.js
@@ -439,6 +439,13 @@ define(function leosArticleIndentListPluginModule(require) {
                     if (originalNumber) {
                         newLi.setAttribute(leosTrackChanges.core.DATA_AKN_TC_ORIGINAL_NUMBER, originalNumber);
                     }
+                    if (leosPluginUtils.calculateListLevel(range.startContainer) === 1) {
+                        newLi.setAttribute(leosPluginUtils.DATA_AKN_NAME, leosPluginUtils.AKN_NUMBERED_PARAGRAPH);
+                        newLi.setAttribute(leosPluginUtils.DATA_AKN_ELEMENT, leosPluginUtils.PARAGRAPH);
+                    } else {
+                        newLi.setAttribute(leosPluginUtils.DATA_AKN_NAME, leosPluginUtils.POINT);
+                        newLi.setAttribute(leosPluginUtils.DATA_AKN_ELEMENT, leosPluginUtils.POINT);
+                    }
                 } else if (!this.isIndent && leosPluginUtils.isSubparagraph(range.startContainer) && range.startContainer.$.nodeName === 'LI' && !range.startContainer.getPrevious()) {
                     // To outdent INTRO subparagraph to paragraph or point
                     var doc = range.startContainer.getParent().getDocument();
