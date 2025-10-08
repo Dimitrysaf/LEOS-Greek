393fae09ae5fe13898e3070c35ec0b77bf52fbb4
camarfe <Felipe.CAMARGO-MACHADO@ext.ec.europa.eu>
2025-10-08 12:50:30 +0000

    Core #2909 Change to fix cypress test

--- modules/js/src/main/js/editor/plugins/leosArticleIndentlist/leosArticleIndentlistPlugin.js
+++ modules/js/src/main/js/editor/plugins/leosArticleIndentlist/leosArticleIndentlistPlugin.js
@@ -502,6 +502,13 @@ define(function leosArticleIndentListPluginModule(require) {
                     if (originalNumber) {
                         newLi.setAttribute(leosTrackChanges.core.DATA_AKN_TC_ORIGINAL_NUMBER, originalNumber);
                     }
+                    if (leosPluginUtils.calculateListLevel(range.startContainer) === 1) {
+                        newLi.setAttribute(leosPluginUtils.DATA_AKN_NAME, leosPluginUtils.AKN_NUMBERED_PARAGRAPH);
+                        newLi.setAttribute(leosPluginUtils.DATA_AKN_ELEMENT, leosPluginUtils.PARAGRAPH);
+                    } else {
+                        newLi.setAttribute(leosPluginUtils.DATA_AKN_NAME, leosPluginUtils.POINT);
+                        newLi.setAttribute(leosPluginUtils.DATA_AKN_ELEMENT, leosPluginUtils.POINT);
+                    }
                 } else if (this.isIndent && leosPluginUtils.isSubparagraph(range.startContainer) && !(range.startContainer.$.nodeName === 'P' && !range.startContainer.getPrevious())) {
                     // To indent subparagraph to point, as the normal indent of paragraph would expand and indent ALL point, not only the paragraph
                     var originalNumber = range.startContainer.getAttribute(leosTrackChanges.core.DATA_AKN_TC_ORIGINAL_NUMBER);
