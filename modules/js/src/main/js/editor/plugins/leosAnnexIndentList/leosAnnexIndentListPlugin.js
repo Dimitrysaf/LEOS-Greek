2818a69ffd17f6eeb088963cb1c50f463e11c706
camarfe <Felipe.CAMARGO-MACHADO@ext.ec.europa.eu>
2025-10-09 12:34:32 +0000

    Core #3024 Fix to wrapper does not appear in annex for level in indent

--- modules/js/src/main/js/editor/plugins/leosAnnexIndentList/leosAnnexIndentListPlugin.js
+++ modules/js/src/main/js/editor/plugins/leosAnnexIndentList/leosAnnexIndentListPlugin.js
@@ -524,6 +524,7 @@ define(function leosAnnexIndentListPluginModule(require) {
         data.listItem.setAttribute("data-akn-num", indentData.nextNum);
         data.listItem.setAttribute(LEOS_ORIGINAL_DEPTH_ATTR, originalDepth);
         editor.fire("handleTcIndent", {data: data.listItem, previousNumber: data.currLvlNum});
+        leosPluginUtils.handleIndentAttributes(data.listItem, editor, true);
         previousDepth = levelItemVo.levelDepth;
         levelItemVo.levelDepth++;
         levelItemVo.levelNum = indentData.nextNum;
