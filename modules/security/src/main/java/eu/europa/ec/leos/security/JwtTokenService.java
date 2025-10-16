e84ec44f1d753f51fa0cbc7c0ec2dea5241e33b7
Andre Restolho <andre-filipe.costa-restolho@ext.ec.europa.eu>
2025-10-16 14:42:08 +0100

    Token

--- modules/security/src/main/java/eu/europa/ec/leos/security/JwtTokenService.java
+++ modules/security/src/main/java/eu/europa/ec/leos/security/JwtTokenService.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -88,6 +89,9 @@ class JwtTokenService implements TokenService {
     @Qualifier("applicationProperties")
     private Properties applicationProperties;
 
+    @Autowired
+    private Environment env;
+
     PasswordConfigurator passwordConfigurator = new PasswordConfigurator();
     
     @Autowired
@@ -104,7 +108,9 @@ class JwtTokenService implements TokenService {
             String clientSecret = passwordConfigurator.getProperty(keyPrefix + clientName + ".secret");
 
             if (clientSecret == null){
-                clientSecret = applicationProperties.getProperty(keyPrefix + clientName + ".secret");
+                LOG.debug("SECRET NOT FOUND");
+                clientSecret = env.getProperty(keyPrefix + clientName + ".secret");
+                LOG.debug("NOTSECRET IS {}", clientSecret);
             }
 
             LOG.debug(clientId);
