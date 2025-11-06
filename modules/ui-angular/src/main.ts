4e6f3016c19a48f0cb32bb352290828d866da2ce
Andre Restolho <andre-filipe.costa-restolho@ext.ec.europa.eu>
2025-11-06 13:09:19 +0000

    Timeout

--- modules/ui-angular/src/main.ts
+++ modules/ui-angular/src/main.ts
@@ -11,7 +11,7 @@ if (environment.production) {
 
 const USE_SERVICE_WORKER = true; // Set to false when you want to remove it
 
-if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator && !window.location.hostname.includes('localhost')) {
   if (USE_SERVICE_WORKER) {
     const baseHref = document.getElementsByTagName('base')[0]?.href || '/';
     const swPath = new URL('ecasSw.js', baseHref).pathname;
