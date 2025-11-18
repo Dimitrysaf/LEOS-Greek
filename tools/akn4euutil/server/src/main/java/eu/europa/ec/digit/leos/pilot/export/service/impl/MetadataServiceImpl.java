09b6cfd45fce5e1803094ed612b0068a2dc7b771
nystepa <pascal.nysten@ec.ext.europa.eu>
2025-11-18 15:30:24 +0100

    Vertical shift

--- tools/akn4euutil/server/src/main/java/eu/europa/ec/digit/leos/pilot/export/service/impl/MetadataServiceImpl.java
+++ tools/akn4euutil/server/src/main/java/eu/europa/ec/digit/leos/pilot/export/service/impl/MetadataServiceImpl.java
@@ -1094,7 +1094,7 @@ public class MetadataServiceImpl implements MetadataService {
             if (disclaimer) {
                 Element coverPageTypeElement = insertElementInCoverPage(xmlFile, MetadataUtil.ELEMENT_DISCLAIMER);
                 if (verticalShift != null) {
-                    XmlUtil.setNodeAttributeValue(coverPageTypeElement, MetadataUtil.STYLE, String.format("margin-top: %scm", verticalShift));
+                    XmlUtil.setNodeAttributeValue(coverPageTypeElement, MetadataUtil.STYLE, String.format("bottom: %scm", 7.0f + verticalShift));
                 }
                 Element coverPageTypePElement = xmlFile.newElement(MetadataUtil.ELEMENT_P);
                 XmlUtil.setNodeAttributeValue(coverPageTypePElement, MetadataUtil.ATTRIBUTE_XMLID, IdGenerator.generateId());
