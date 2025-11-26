0244dc913345c252aa4591347dc9ba1a1cf67512
rizwamo <RIZWAN-Mohammed.Abdul@ext.ec.europa.eu>
2025-11-26 16:35:52 +0100

    Core:#3209 Automation Test Case for insertspecialchar

--- modules/test-cypress/cypress/e2e/step_definitions/actViewerSteps.js
+++ modules/test-cypress/cypress/e2e/step_definitions/actViewerSteps.js
@@ -152,7 +152,6 @@ Then(/^eea Relevance is ticked in details tab$/, function () {
 });
 
 Then(/^EEA Relevance is unticked in details tab$/, function () {
-    cy.wait(2000);
     actViewerPage.elements.eeARelevanceCheckBoxValue().should('not.be.checked');
 });
 
