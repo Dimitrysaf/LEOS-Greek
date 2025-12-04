a26ae2f2fd62a933b8318e6ea59e5c3f02f3520c
krishpa <Pandiyarajan.KRISHNAMURTHY@ext.ec.europa.eu>
2025-12-04 10:39:58 +0100

    add comment for the method

--- modules/services/src/main/java/eu/europa/ec/leos/services/store/WorkspaceOptions.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/store/WorkspaceOptions.java
@@ -67,7 +67,7 @@ public class WorkspaceOptions {
     }
 
     private void customTemplateFilter() {
-        //set false if user is not having Template manager or Support Role
+        //Set the value to false if the user does not have either the Template Manager role or the Support role.
         if (!(securityContext.getUser().getRoles().contains("TEMPLATE_MANAGER")
                 || securityContext.getUser().getRoles().contains("SUPPORT"))) {
             workspaceFilter.addFilter(new QueryFilter.Filter(FilterType.customTemplates.name(),
