da3bfbdfa759f279a6e428f666614d80b5a5ef02
camarfe <Felipe.CAMARGO-MACHADO@ext.ec.europa.eu>
2025-12-05 09:48:23 +0000

    Core #3144 Undo previous test

--- modules/ui-angular/src/app/features/proposal-view/services/proposal-details.service.ts
+++ modules/ui-angular/src/app/features/proposal-view/services/proposal-details.service.ts
@@ -12,7 +12,7 @@ import {
   ErrorCode,
   LeosAppConfig,
   Permission,
-  User, LegFileValidationResponse
+  User
 } from '@leos/shared';
 import { TranslateService } from '@ngx-translate/core';
 import { parse as parseContentDisposition } from 'content-disposition-attachment';
