2bed538b9ef22c132b7d247eb6a26dc809e2c549
krishpa <Pandiyarajan.KRISHNAMURTHY@ext.ec.europa.eu>
2025-12-05 19:12:58 +0100

    add comment

--- modules/services/src/main/java/eu/europa/ec/leos/services/store/WorkspaceOptions.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/store/WorkspaceOptions.java
@@ -68,7 +68,7 @@ public class WorkspaceOptions {
 
     private void customTemplateFilter() {
         boolean hasCreateTemplatePermission = securityContext.hasPermission(null, LeosPermission.CAN_CREATE_TEMPLATE);
-        //Set the value to false if the user does not have create template permission
+        //Set the value to false if the user does not have create template permission.
         if (!(hasCreateTemplatePermission)) {
             workspaceFilter.addFilter(new QueryFilter.Filter(FilterType.customTemplates.name(),
                     "=", false, true, "false"));
