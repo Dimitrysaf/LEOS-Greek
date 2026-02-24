ec99e40f8b56abec52075ce7fdb07f62c7f40ba8
Andre Restolho <andre-filipe.costa-restolho@ext.ec.europa.eu>
2026-02-24 09:19:36 +0000

    3188- Fixed beans being instantiated twice.

--- modules/services/src/main/java/eu/europa/ec/leos/services/controllers/LeosApiController.java
+++ modules/services/src/main/java/eu/europa/ec/leos/services/controllers/LeosApiController.java
@@ -64,6 +64,7 @@ import eu.europa.ec.leos.vo.coedition.CoEditionVO;
 import eu.europa.ec.leos.vo.coedition.InfoType;
 import eu.europa.ec.leos.vo.token.JsonTokenReponse;
 import eu.europa.ec.leos.model.notification.validation.DocumentExternalValidationNotification;
+import jakarta.annotation.PostConstruct;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -152,6 +153,14 @@ public class LeosApiController {
 
     @Value("${notification.functional.mailbox}")
     private String notificationRecipient;
+
+    @PostConstruct
+    public void init() {
+        LOG.info("Initializing LeosApiController...");
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("LeosApiController initialized.");
+        }
+    }
 
     @Autowired
     public LeosApiController(LegService legService, WorkspaceService workspaceService, TokenService tokenService,
