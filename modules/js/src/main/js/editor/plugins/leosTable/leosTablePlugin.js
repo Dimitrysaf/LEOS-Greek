bf4ada6af039496524609104537b4fccf9be5a5a
ANTON Marius <Marius.Anton@ext.ec.europa.eu>
2026-02-26 17:13:04 +0000

    Core #3143 - fix validation of function

--- modules/js/src/main/js/editor/plugins/leosTable/leosTablePlugin.js
+++ modules/js/src/main/js/editor/plugins/leosTable/leosTablePlugin.js
@@ -318,11 +318,11 @@ define(function leosTablePluginModule(require) {
 
         // Find closest <td> or <th> ancestor for start and end
         var startCell = startNode.getAscendant(function(el) {
-            return el.is('td') || el.is('th');
+            return el && (typeof el.is === 'function') && (el.is('td') || el.is('th'));
         }, true);
 
         var endCell = endNode.getAscendant(function(el) {
-            return el.is('td') || el.is('th');
+            return !!el && (typeof el.is === 'function') && (el.is('td') || el.is('th'));
         }, true);
 
         // Both must exist and be exactly the same cell
