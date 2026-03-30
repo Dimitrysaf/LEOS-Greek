50f6621d8e1b83defbb597a4461ee700a97ec534
khandsa <sapna.khandelwal@ext.ec.europa.eu>
2026-03-30 10:49:39 +0200

    Core #3590 : Automation test case

--- modules/test-cypress/cypress/e2e/pages/ckEditorWindow.js
+++ modules/test-cypress/cypress/e2e/pages/ckEditorWindow.js
@@ -712,19 +712,10 @@ class ckEditorWindow {
                 let editor = w.CKEDITOR.instances[Object.keys(w.CKEDITOR.instances)[0]];
                 let elementToPutCursorParent = editor.element.findOne(element);
                 let elementToPutCursor = elementToPutCursorParent.getChild(child);
-                let textNode;
-
-                if (elementToPutCursor.type === w.CKEDITOR.NODE_TEXT) {
-                    textNode = elementToPutCursor;
-                } else {
-                    textNode = elementToPutCursor.getFirst(e => e.type === w.CKEDITOR.NODE_TEXT);
-                }
-                let textLength = textNode.getText().length;
-                let safeOffset = Math.min(offset, textLength);
                 let range = editor.createRange();
                 range.moveToPosition(elementToPutCursor, w.CKEDITOR.POSITION_AFTER_START);
-                range.setStart(textNode, safeOffset);
-                range.setEnd(textNode, safeOffset);
+                range.setStart(elementToPutCursor, offset);
+                range.setEnd(elementToPutCursor, offset);
                 range.collapse(true);
                 // range.select();
                 // Force selection to this range
